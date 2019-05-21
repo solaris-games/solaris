3615125a7848313f789a2f1613e6a0e59adfaede
Michael Eason <admin@voxelsoftware.co.uk>
2019-05-21 21:10:27 +0100

    Add logout button to main menu

--- client/src/views/MainMenu.vue
+++ client/src/views/MainMenu.vue
@@ -17,12 +17,24 @@
     <div>
       <router-link to="/premium-store" tag="button">Premium Store</router-link>
     </div>
+
+    <div>
+      <button @click="logout">Logout</button>
+    </div>
   </div>
 </template>
 
 <script>
+import router from '../router';
+
 export default {
-    
+    methods: {
+        logout() {
+            // TODO: Log the user out.
+
+            router.push({ name: 'home' });
+        }
+    }
 };
 </script>
 
