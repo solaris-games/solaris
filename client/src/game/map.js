593193418bffed6f89f488fddcd006031a41abd0
Michael Eason <admin@voxelsoftware.co.uk>
2019-06-08 20:02:37 +0100

    Draw star names.

--- client/src/game/map.js
+++ client/src/game/map.js
@@ -11,10 +11,23 @@ function drawStars(game) {
     for (let i = 0; i < game.galaxy.stars.length; i++) {
         let star = game.galaxy.stars[i];
 
+        // Draw the star itself.
         graphics.lineStyle(0); // draw a circle, set the lineStyle to zero so the circle doesn't have an outline
         graphics.beginFill(0xFFFFFF, 1);
         graphics.drawCircle(star.location.x, star.location.y, 2);
         graphics.endFill();
+
+        // Draw the star name.
+        let text = new PIXI.Text(star.name, {
+            fontSize: 8,
+            fill: 0xFFFFFF
+        });
+
+        text.x = star.location.x - (text.width / 2);
+        text.y = star.location.y + 3;
+        text.resolution = 10;
+
+        container.addChild(text);
     }
 
     container.addChild(graphics);
