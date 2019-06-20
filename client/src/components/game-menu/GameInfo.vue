9315b4bce9c9fe48402f114f7132e50d5743442c
Michael Eason <admin@voxelsoftware.co.uk>
2019-06-20 20:46:13 +0100

    Add framework for game menu navigation.

--- client/src/components/game-menu/GameInfo.vue
+++ client/src/components/game-menu/GameInfo.vue
@@ -2,10 +2,19 @@
 <div class="bg-primary">
     <div class="container">
         <div class="row no-gutters pt-2 pb-2">
-            <div class="col-1">
-                <button class="btn btn-sm btn-info">
+            <div class="col-1 dropdown">
+                <button class="btn btn-sm btn-info" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="fas fa-bars"></i>
                 </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" v-on:click="setMenuState(MENU_STATES.LEADERBOARD)"><i class="fas fa-users mr-2"></i>Leaderboard</a>
+                    <a class="dropdown-item" v-on:click="setMenuState(MENU_STATES.RESEARCH)"><i class="fas fa-flask mr-2"></i>Research</a>
+                    <a class="dropdown-item" v-on:click="setMenuState(MENU_STATES.GALAXY)"><i class="fas fa-star mr-2"></i>Galaxy</a>
+                    <a class="dropdown-item" v-on:click="setMenuState(MENU_STATES.INTEL)"><i class="fas fa-chart-line mr-2"></i>Intel</a>
+                    <a class="dropdown-item" v-on:click="setMenuState(MENU_STATES.OPTIONS)"><i class="fas fa-cog mr-2"></i>Options</a>
+                    <a class="dropdown-item" v-on:click="setMenuState(MENU_STATES.HELP)"><i class="fas fa-question mr-2"></i>Help</a>
+                    <a class="dropdown-item" v-on:click="goToMainMenu()"><i class="fas fa-chevron-left mr-2"></i>Main Menu</a>
+                </div>
             </div>
             <div class="col-4 text-center">
                 <span class="align-middle">Credits: ${{credits}}</span>
@@ -14,7 +23,7 @@
                 <span class="align-middle">Production: {{timeRemaining}}</span>
             </div>
             <div class="col-1">
-                <button class="btn btn-sm btn-info">
+                <button class="btn btn-sm btn-info" v-on:click="setMenuState(MENU_STATES.INBOX)">
                     <i class="fas fa-inbox"></i>
                 </button>
             </div>
@@ -24,16 +33,20 @@
 </template>
 
 <script>
+import router from '../../router';
 import { setInterval } from 'timers';
+import MENU_STATES from '../data/menuStates';
 
 export default {
     props: {
         credits: Number,
-        nextProduction: Date
+        nextProduction: Date,
+        onMenuStateChanged: Function
     },
     data() {
         return {
-            forceRecomputeCounter: 0 // Need to use this hack to force vue to recalculate the time remaining
+            forceRecomputeCounter: 0, // Need to use this hack to force vue to recalculate the time remaining
+            MENU_STATES: MENU_STATES
         };
     },
     mounted() {
@@ -53,6 +66,17 @@ export default {
             let secs = Math.floor((t % (1000 * 60)) / 1000);
 
             return `${days}d ${hours}h ${mins}m ${secs}s`;
+        }
+    },
+    methods: {
+        setMenuState(state, args) {
+            this.$emit('onMenuStateChanged', {
+                state,
+                args
+            });
+        },
+        goToMainMenu() {
+            router.push({ name: 'main-menu' });
         }
     }
 }
--- client/src/components/game-menu/MainBar.vue
+++ client/src/components/game-menu/MainBar.vue
@@ -1,13 +1,23 @@
 <template>
 <div>
-    <game-info v-bind:credits="500" v-bind:nextProduction="new Date().setHours(new Date().getHours() + 1)"/>
+    <game-info v-bind:credits="500" v-bind:nextProduction="new Date().setHours(new Date().getHours() + 1)" @onMenuStateChanged="onMenuStateChanged"/>
+
     <player-list v-bind:players="game.galaxy.players"/>
+
+    <div v-if="currentMenuState == MENU_STATES.LEADERBOARD">LEADERBOARD</div>
+    <div v-if="currentMenuState == MENU_STATES.RESEARCH">RESEARCH</div>
+    <div v-if="currentMenuState == MENU_STATES.GALAXY">GALAXY</div>
+    <div v-if="currentMenuState == MENU_STATES.INTEL">INTEL</div>
+    <div v-if="currentMenuState == MENU_STATES.OPTIONS">OPTIONS</div>
+    <div v-if="currentMenuState == MENU_STATES.HELP">HELP</div>
+    <div v-if="currentMenuState == MENU_STATES.INBOX">INBOX</div>
 </div>
 </template>
 
 <script>
 import GameInfo from './GameInfo.vue';
 import PlayerList from './PlayerList.vue';
+import MENU_STATES from '../data/menuStates';
 
 export default {
     components: {
@@ -16,6 +26,19 @@ export default {
     },
     props: {
         game: Object
+    },
+    data() {
+        return {
+            currentMenuState: null,
+            currentMenuArguments: null,
+            MENU_STATES: MENU_STATES
+        };
+    },
+    methods: {
+        onMenuStateChanged(e) {
+            this.currentMenuState = e.state || null;
+            this.currentMenuArguments = e.args || null;
+        }
     }
 }
 </script>
