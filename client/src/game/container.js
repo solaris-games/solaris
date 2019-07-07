fa20e0a115f3c5dd9e80012d6a381d5c19795b94
Michael Eason <admin@voxelsoftware.co.uk>
2019-07-07 13:06:00 +0100

    You would not believe how long it has taken to fix this bug.

--- client/src/game/container.js
+++ client/src/game/container.js
@@ -32,8 +32,9 @@ class GameContainer {
             screenWidth: window.innerWidth,
             screenHeight: window.innerHeight,
 
-            // worldWidth: this._calculateMaxStarX(game),// - this._calculateMinStarX(game),
-            // worldHeight: this._calculateMaxStarY(game),// - this._calculateMinStarY(game),
+            // yolo
+            worldWidth: Number.MAX_VALUE,
+            worldHeight: Number.MAX_VALUE,
         
             interaction: this.app.renderer.plugins.interaction // the interaction module is important for wheel() to work properly when renderer.view is placed or scaled
         });
