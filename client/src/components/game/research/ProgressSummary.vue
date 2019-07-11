c64a272b91f110657cc6ae8632c53b40406d8ff7
Michael Eason <admin@voxelsoftware.co.uk>
2019-07-11 22:10:25 +0100

    Add basic research progress screen

--- client/src/components/game/research/ProgressSummary.vue
+++ client/src/components/game/research/ProgressSummary.vue
@@ -1,6 +1,46 @@
 <template>
-<div class="row">
-    progress
+<div class="row bg-primary">
+    <div class="col">
+    <table class="table table-sm mb-0">
+        <tbody>
+            <tr>
+                <td>Scanning</td>
+                <td>Level 1</td>
+                <td class="text-right">0 of 0</td>
+            </tr>
+            <tr>
+                <td>Hyperspace Range</td>
+                <td>Level 1</td>
+                <td class="text-right">0 of 0</td>
+            </tr>
+            <tr>
+                <td>Terraforming</td>
+                <td>Level 1</td>
+                <td class="text-right">0 of 0</td>
+            </tr>
+            <tr>
+                <td>Experimentation</td>
+                <td>Level 1</td>
+                <td class="text-right">0 of 0</td>
+            </tr>
+            <tr>
+                <td>Weapons</td>
+                <td>Level 1</td>
+                <td class="text-right">0 of 0</td>
+            </tr>
+            <tr>
+                <td>Banking</td>
+                <td>Level 1</td>
+                <td class="text-right">0 of 0</td>
+            </tr>
+            <tr>
+                <td>Manufacturing</td>
+                <td>Level 1</td>
+                <td class="text-right">0 of 0</td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
 </div>
 </template>
 
