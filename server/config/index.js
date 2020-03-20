8e15f5673b58d4a69bfe424d4f3a597157f42a96
Mike Eason <mike.eason@autointegrate.com>
2020-03-20 13:44:34 +0000

    Add client URL to server config variables. closes #53

--- server/config/index.js
+++ server/config/index.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
     port: process.env.PORT,
     sessionSecret: process.env.SESSION_SECRET,
-    connectionString: process.env.CONNECTION_STRING
+    connectionString: process.env.CONNECTION_STRING,
+    clientUrl: process.env.CLIENT_URL
 };
--- server/loaders/express.js
+++ server/loaders/express.js
@@ -14,7 +14,7 @@ module.exports = async (app) => {
 
     // Enable CORS
     app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+        res.header("Access-Control-Allow-Origin", config.clientUrl);
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.header('Access-Control-Allow-Headers', 'Content-Type');
         res.header('Access-Control-Allow-Credentials', 'true');
