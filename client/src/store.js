c4edffac90f823c8007423c9b853acb8fb1a137a
Mike Eason <mike.eason@mail.com>
2020-04-25 14:36:53 +0100

    Made somewhat of a home page

--- client/src/store.js
+++ client/src/store.js
@@ -5,7 +5,7 @@ import VuexPersist from 'vuex-persist'
 Vue.use(Vuex)
 
 const vuexPersist = new VuexPersist({
-  key: 'np-clone',
+  key: 'solaris',
   storage: localStorage
 })
 
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,23 @@
 <template>
   <view-container>
-    <h1>Home</h1>
+    <h1 class="pt-3">Solaris</h1>
 
-    <div>
-      <router-link to="/account/create" tag="button" class="btn btn-primary">Create A New Account</router-link>
-      <router-link to="/account/login" tag="button" class="btn btn-success ml-1">Login</router-link>
+    <p>The multiplayer game of Strategy, Intrigue and Galactic Conquest.</p>
+
+    <div class="row">
+        <div class="col">
+          <router-link to="/account/create" tag="button" class="btn btn-block btn-primary">Create Account</router-link>
+        </div>
+        <div class="col">
+          <router-link to="/account/login" tag="button" class="btn btn-block btn-success">Login</router-link>
+        </div>
+    </div>
+
+    <img />
+
+    <div class="text-center">
+      <h4>A board game for the web!</h4>
+      <p>Login throughout the day to command epic fleets of spacecraft in a desperate struggle to conquer the galaxy!</p>
     </div>
   </view-container>
 </template>
