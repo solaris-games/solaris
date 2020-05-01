ac59bd004db54e1d81d1c456bb546a33f610e59f
Mike Eason <mike.eason@autointegrate.com>
2020-05-01 17:12:23 +0100

    Redirect to login page if 401s are returned from the API. closes #57

--- client/src/services/api/base.js
+++ client/src/services/api/base.js
@@ -1,8 +1,24 @@
+import axios from 'axios'
+import router from '../../router'
+
 class BaseApiService {
 
     // TODO: Needs to be in an env config setting.
     BASE_URL = 'http://localhost:3000/api/'
 
+    constructor () {
+        axios.interceptors.response.use(
+            response => {
+                return response
+            }, error => {
+                // If any Unathorized responses come back, redirect to login page.
+                if (error.response.status === 401) {
+                    router.push({ name: 'account-login' })
+                }
+
+                return Promise.reject({ ...error })
+            })
+    }
 }
 
 export default BaseApiService
