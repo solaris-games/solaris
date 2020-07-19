3f7e742490df77886274da8d949319b4b23617f9
Mike Eason <mike.eason@autointegrate.com>
2020-07-19 22:17:02 +0100

    Add charts to achievements page

---
+++ client/src/components/game/intel/PolarAreaChart.js
@@ -0,0 +1,16 @@
+import { PolarArea, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+
+export default {
+  extends: PolarArea,
+  mixins: [reactiveProp],
+  props: ['options'],
+  mounted () {
+    // eslint-disable-next-line
+    Chart.defaults.global.defaultFontColor = '#fff'
+
+    // this.chartData is created in the mixin.
+    // If you want to pass options please create a local options object
+    this.renderChart(this.chartData, this.options)
+  }
+}
--- client/src/views/AccountAchievements.vue
+++ client/src/views/AccountAchievements.vue
@@ -10,7 +10,8 @@
     <p class="text-center pt-3 mb-0">Read more about <a href="javascript:;">Victory, Rank and Renown</a>.</p>
 
     <h5>Games</h5>
-    <div class="table-responsive" v-if="user">
+    <div class="row" v-if="user">
+      <div class="col-sm-12 col-md-8 table-responsive">
       <table class="table table-striped table-hover">
         <tbody>
           <tr>
@@ -39,126 +40,158 @@
           </tr>
         </tbody>
       </table>
+      </div>
+      <div class="d-none d-md-block col-4">
+        <polar-area-chart v-if="gamesChartData" :chart-data="gamesChartData" :options="pieChartOptions" />
+      </div>
     </div>
 
     <h5>Military</h5>
-    <div class="table-responsive" v-if="user">
-      <table class="table table-striped table-hover">
-        <tbody>
-          <tr>
-            <td>Ship Kills</td>
-            <td class="text-right">{{ user.achievements.combat.kills.ships }}</td>
-          </tr>
-          <tr>
-            <td>Ship Losses</td>
-            <td class="text-right">{{ user.achievements.combat.losses.ships }}</td>
-          </tr>
-          <tr>
-            <td>Carrier Kills</td>
-            <td class="text-right">{{ user.achievements.combat.kills.carriers }}</td>
-          </tr>
-          <tr>
-            <td>Carrier Losses</td>
-            <td class="text-right">{{ user.achievements.combat.losses.carriers }}</td>
-          </tr>
-          <tr>
-            <td>Stars Captured</td>
-            <td class="text-right">{{ user.achievements.combat.stars.captured }}</td>
-          </tr>
-          <tr>
-            <td>Stars Lost</td>
-            <td class="text-right">{{ user.achievements.combat.stars.lost }}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="row" v-if="user">
+      <div class="col-sm-12 col-md-8 table-responsive">
+        <table class="table table-striped table-hover">
+          <tbody>
+            <tr>
+              <td>Ship Kills</td>
+              <td class="text-right">{{ user.achievements.combat.kills.ships }}</td>
+            </tr>
+            <tr>
+              <td>Ship Losses</td>
+              <td class="text-right">{{ user.achievements.combat.losses.ships }}</td>
+            </tr>
+            <tr>
+              <td>Carrier Kills</td>
+              <td class="text-right">{{ user.achievements.combat.kills.carriers }}</td>
+            </tr>
+            <tr>
+              <td>Carrier Losses</td>
+              <td class="text-right">{{ user.achievements.combat.losses.carriers }}</td>
+            </tr>
+            <tr>
+              <td>Stars Captured</td>
+              <td class="text-right">{{ user.achievements.combat.stars.captured }}</td>
+            </tr>
+            <tr>
+              <td>Stars Lost</td>
+              <td class="text-right">{{ user.achievements.combat.stars.lost }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="d-none d-md-block col-4">
+        <pie-chart v-if="militaryChartData" :chart-data="militaryChartData" :options="militaryChartOptions" />
+      </div>
     </div>
 
     <h5>Infrastructure</h5>
-    <div class="table-responsive" v-if="user">
-      <table class="table table-striped table-hover">
-        <tbody>
-          <tr>
-            <td>Economy</td>
-            <td class="text-right">{{ user.achievements.infrastructure.economy }}</td>
-          </tr>
-          <tr>
-            <td>Industry</td>
-            <td class="text-right">{{ user.achievements.infrastructure.industry }}</td>
-          </tr>
-          <tr>
-            <td>Science</td>
-            <td class="text-right">{{ user.achievements.infrastructure.science }}</td>
-          </tr>
-          <tr>
-            <td>Warp Gates Built</td>
-            <td class="text-right">{{ user.achievements.infrastructure.warpGates }}</td>
-          </tr>
-          <tr>
-            <td>Warp Gates Destroyed</td>
-            <td class="text-right">{{ user.achievements.infrastructure.warpGatesDestroyed }}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="row" v-if="user">
+      <div class="col-sm-12 col-md-8 table-responsive">
+        <table class="table table-striped table-hover">
+          <tbody>
+            <tr>
+              <td>Economy</td>
+              <td class="text-right">{{ user.achievements.infrastructure.economy }}</td>
+            </tr>
+            <tr>
+              <td>Industry</td>
+              <td class="text-right">{{ user.achievements.infrastructure.industry }}</td>
+            </tr>
+            <tr>
+              <td>Science</td>
+              <td class="text-right">{{ user.achievements.infrastructure.science }}</td>
+            </tr>
+            <tr>
+              <td>Warp Gates Built</td>
+              <td class="text-right">{{ user.achievements.infrastructure.warpGates }}</td>
+            </tr>
+            <tr>
+              <td>Warp Gates Destroyed</td>
+              <td class="text-right">{{ user.achievements.infrastructure.warpGatesDestroyed }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="d-none d-md-block col-4">
+        <pie-chart v-if="infrastructureChartData" :chart-data="infrastructureChartData" :options="pieChartOptions" />
+      </div>
     </div>
-
+    
     <h5>Research</h5>
-    <div class="table-responsive" v-if="user">
-      <table class="table table-striped table-hover">
-        <tbody>
-          <tr>
-            <td>Scanning</td>
-            <td class="text-right">{{ user.achievements.research.scanning }}</td>
-          </tr>
-          <tr>
-            <td>Hyperspace</td>
-            <td class="text-right">{{ user.achievements.research.hyperspace }}</td>
-          </tr>
-          <tr>
-            <td>Terraforming</td>
-            <td class="text-right">{{ user.achievements.research.terraforming }}</td>
-          </tr>
-          <tr>
-            <td>Weapons</td>
-            <td class="text-right">{{ user.achievements.research.weapons }}</td>
-          </tr>
-          <tr>
-            <td>Banking</td>
-            <td class="text-right">{{ user.achievements.research.banking }}</td>
-          </tr>
-          <tr>
-            <td>Manufacturing</td>
-            <td class="text-right">{{ user.achievements.research.manufacturing }}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="row" v-if="user">
+      <div class="col-sm-12 col-md-8 table-responsive">
+        <table class="table table-striped table-hover">
+          <tbody>
+            <tr>
+              <td>Scanning</td>
+              <td class="text-right">{{ user.achievements.research.scanning }}</td>
+            </tr>
+            <tr>
+              <td>Hyperspace</td>
+              <td class="text-right">{{ user.achievements.research.hyperspace }}</td>
+            </tr>
+            <tr>
+              <td>Terraforming</td>
+              <td class="text-right">{{ user.achievements.research.terraforming }}</td>
+            </tr>
+            <tr>
+              <td>Weapons</td>
+              <td class="text-right">{{ user.achievements.research.weapons }}</td>
+            </tr>
+            <tr>
+              <td>Banking</td>
+              <td class="text-right">{{ user.achievements.research.banking }}</td>
+            </tr>
+            <tr>
+              <td>Manufacturing</td>
+              <td class="text-right">{{ user.achievements.research.manufacturing }}</td>
+            </tr>
+            <tr>
+              <td>Experimentation</td>
+              <td class="text-right">{{ user.achievements.research.experimentation }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="d-none d-md-block col-4">
+        <pie-chart v-if="researchChartData" :chart-data="researchChartData" :options="pieChartOptions" />
+      </div>
     </div>
 
     <h5>Trade</h5>
-    <div class="table-responsive" v-if="user">
-      <table class="table table-striped table-hover">
-        <tbody>
-          <tr>
-            <td>Credits Sent</td>
-            <td class="text-right">{{ user.achievements.trade.creditsSent }}</td>
-          </tr>
-          <tr>
-            <td>Credits Received</td>
-            <td class="text-right">{{ user.achievements.trade.creditsReceived }}</td>
-          </tr>
-          <tr>
-            <td>Technology Sent</td>
-            <td class="text-right">{{ user.achievements.trade.technologySent }}</td>
-          </tr>
-          <tr>
-            <td>Technology Received</td>
-            <td class="text-right">{{ user.achievements.trade.technologyReceived }}</td>
-          </tr>
-          <tr>
-            <td>Renown Sent</td>
-            <td class="text-right">{{ user.achievements.trade.renownSent }}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="row" v-if="user">
+      <div class="col-sm-12 col-md-8 table-responsive">
+        <table class="table table-striped table-hover">
+          <tbody>
+            <tr>
+              <td>Credits Sent</td>
+              <td class="text-right">{{ user.achievements.trade.creditsSent }}</td>
+            </tr>
+            <tr>
+              <td>Credits Received</td>
+              <td class="text-right">{{ user.achievements.trade.creditsReceived }}</td>
+            </tr>
+            <tr>
+              <td>Technology Sent</td>
+              <td class="text-right">{{ user.achievements.trade.technologySent }}</td>
+            </tr>
+            <tr>
+              <td>Technology Received</td>
+              <td class="text-right">{{ user.achievements.trade.technologyReceived }}</td>
+            </tr>
+            <tr>
+              <td>Renown Sent</td>
+              <td class="text-right">{{ user.achievements.trade.renownSent }}</td>
+            </tr>
+            <tr>
+              <td>Renown Received</td>
+              <td class="text-right">{{ user.achievements.renown }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="d-none d-md-block col-4">
+        <pie-chart v-if="tradeChartData" :chart-data="tradeChartData" :options="tradeChartOptions" />
+      </div>
     </div>
 
     <!--
@@ -329,6 +362,8 @@ import ViewContainer from '../components/ViewContainer'
 import ViewTitle from '../components/ViewTitle'
 import ViewSubtitle from '../components/ViewSubtitle'
 import Achievements from '../components/game/player/Achievements'
+import PieChart from '../components/game/intel/PieChart.js'
+import PolarArea from '../components/game/intel/PolarAreaChart.js'
 import userService from '../services/api/user'
 
 export default {
@@ -337,12 +372,58 @@ export default {
     'view-container': ViewContainer,
     'view-title': ViewTitle,
     'view-subtitle': ViewSubtitle,
-    'achievements': Achievements
+    'achievements': Achievements,
+    'pie-chart': PieChart,
+    'polar-area-chart': PolarArea
   },
   data () {
     return {
       user: null,
-      achievements: null
+      achievements: null,
+      militaryChartData: null,
+      infrastructureChartData: null,
+      researchChartData: null,
+      pieChartOptions: {
+        legend: {
+          display: false
+        }
+      },
+      militaryChartOptions: {
+        legend: {
+          display: false
+        },
+        tooltips: {
+          callbacks: {
+            title: function(tooltipItems, data) {
+              return data.datasets[tooltipItems[0].datasetIndex].label;
+            },
+            label: function(tooltipItem, data) {
+              // If the star dataset, use the label "Captured"
+              // instead of "Kills"
+              let label = data.labels[tooltipItem.index]
+
+              if (tooltipItem.datasetIndex === 2) {
+                label = tooltipItem.index ? 'Losses' : 'Captured'
+              }
+              
+              return label + ': ' +
+                data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index]
+            }
+          }
+        }
+      },
+      tradeChartOptions: {
+        legend: {
+          display: false
+        },
+        tooltips: {
+          callbacks: {
+            title: function(tooltipItems, data) {
+              return data.datasets[tooltipItems[0].datasetIndex].label;
+            }
+          }
+        }
+      }
     }
   },
   async mounted () {
@@ -350,8 +431,208 @@ export default {
       let response = await userService.getUserAchievements(this.$route.params.userId)
 
       this.user = response.data
+
+      this.loadGamesChart()
+      this.loadMilitaryChart()
+      this.loadInfrastructureChart()
+      this.loadResearchChart()
+      this.loadTradeChart()
     } catch (err) {
       console.error(err)
+    }
+  },
+  methods: {
+    loadGamesChart () {
+      this.gamesChartData = null
+
+      let chartData = {
+        labels: [
+          'Victories',
+          'Joined',
+          'Completed',
+          'Defeated',
+          'Quit',
+          'AFK'
+        ],
+        datasets: [
+          {
+            data: [
+              this.user.achievements.victories,
+              this.user.achievements.joined,
+              this.user.achievements.completed,
+              this.user.achievements.defeated,
+              this.user.achievements.quit,
+              this.user.achievements.afk
+            ],
+            backgroundColor: [
+              '#00bc8c',
+              '#375a7f',
+              '#3498DB',
+              '#F39C12',
+              '#444',
+              '#E74C3C'
+            ]
+          }
+        ]
+      }
+
+      this.gamesChartData = chartData
+    },
+    loadMilitaryChart () {
+      this.militaryChartData = null
+
+      let chartData = {
+        labels: [
+          'Kills',
+          'Losses'
+        ],
+        datasets: [
+          {
+            data: [
+              this.user.achievements.combat.kills.ships,
+              this.user.achievements.combat.losses.ships
+            ],
+            backgroundColor: [
+              '#00bc8c',
+              '#E74C3C'
+            ],
+            label: 'Ships'
+          },
+          {
+            data: [
+              this.user.achievements.combat.kills.carriers,
+              this.user.achievements.combat.losses.carriers
+            ],
+            backgroundColor: [
+              '#00bc8c',
+              '#E74C3C'
+            ],
+            label: 'Carriers'
+          },
+          {
+            data: [
+              this.user.achievements.combat.stars.captured,
+              this.user.achievements.combat.stars.lost
+            ],
+            backgroundColor: [
+              '#00bc8c',
+              '#E74C3C'
+            ],
+            label: 'Stars'
+          }
+        ]
+      }
+
+      this.militaryChartData = chartData
+    },
+    loadInfrastructureChart () {
+      this.infrastructureChartData = null
+
+      let chartData = {
+        labels: [
+          'Economy',
+          'Industry',
+          'Science'
+        ],
+        datasets: [{
+          data: [
+            this.user.achievements.infrastructure.economy,
+            this.user.achievements.infrastructure.industry,
+            this.user.achievements.infrastructure.science,
+          ],
+          backgroundColor: [
+            '#00bc8c',
+            '#F39C12',
+            '#3498DB'
+          ]
+        }]
+      }
+
+      this.infrastructureChartData = chartData
+    },
+    loadResearchChart () {
+      this.researchChartData = null
+
+      let chartData = {
+        labels: [
+          'Scanning',
+          'Hyperspace',
+          'Terraforming',
+          'Weapons',
+          'Banking',
+          'Manufacturing',
+          'Experimentation',
+        ],
+        datasets: [{
+          data: [
+            this.user.achievements.research.scanning,
+            this.user.achievements.research.hyperspace,
+            this.user.achievements.research.terraforming,
+            this.user.achievements.research.weapons,
+            this.user.achievements.research.banking,
+            this.user.achievements.research.manufacturing,
+            this.user.achievements.research.experimentation
+          ],
+          backgroundColor: [
+            '#888',
+            '#375a7f',
+            '#444',
+            '#F39C12',
+            '#E74C3C',
+            '#00bc8c',
+            '#3498DB'
+          ]
+        }]
+      }
+
+      this.researchChartData = chartData
+    },
+    loadTradeChart () {
+      this.tradeChartData = null
+
+      let chartData = {
+        labels: [
+          'Sent',
+          'Received'
+        ],
+        datasets: [
+          {
+            data: [
+              this.user.achievements.trade.creditsSent,
+              this.user.achievements.trade.creditsReceived
+            ],
+            backgroundColor: [
+              '#00bc8c',
+              '#3498DB'
+            ],
+            label: 'Credits'
+          },
+          {
+            data: [
+              this.user.achievements.trade.technologySent,
+              this.user.achievements.trade.technologyReceived
+            ],
+            backgroundColor: [
+              '#00bc8c',
+              '#3498DB'
+            ],
+            label: 'Technology'
+          },
+          {
+            data: [
+              this.user.achievements.trade.renownSent,
+              this.user.achievements.renown
+            ],
+            backgroundColor: [
+              '#00bc8c',
+              '#3498DB'
+            ],
+            label: 'Renown'
+          }
+        ]
+      }
+
+      this.tradeChartData = chartData
     }
   }
 }
