79514945e578bbc6e8599aeb034184f9c4fa6233
Mike Eason <mike.eason@autointegrate.com>
2020-09-18 09:32:07 +0100

    Add ability to choose a random tech for next research, closes #309

--- server/services/research.js
+++ server/services/research.js
@@ -13,6 +13,8 @@ module.exports = class ResearchService extends EventEmitter {
     }
 
     async updateResearchNow(game, player, preference) {
+        preference = preference.toLowerCase().trim();
+
         if (!this.technologyService.isTechnologyEnabled(game, preference)) {
             throw new ValidationError(`Cannot change technology, the chosen tech is not researchable.`);
         }
@@ -29,7 +31,9 @@ module.exports = class ResearchService extends EventEmitter {
     }
 
     async updateResearchNext(game, player, preference) {
-        if (!this.technologyService.isTechnologyEnabled(game, preference)) {
+        preference = preference.toLowerCase().trim();
+
+        if (preference !== 'random' && !this.technologyService.isTechnologyEnabled(game, preference)) {
             throw new ValidationError(`Cannot change technology, the chosen tech is not researchable.`);
         }
 
@@ -70,7 +74,13 @@ module.exports = class ResearchService extends EventEmitter {
                 technology: {name:techKey,level:tech.level}
             });
 
-            player.researchingNow = player.researchingNext;
+            if (player.researchingNext === 'random') {
+                let randomTech = this._getRandomTechnology(game, player);
+
+                player.researchingNow = randomTech.key;
+            } else {
+                player.researchingNow = player.researchingNext;
+            }
 
             levelUp = true
         }
@@ -106,35 +116,26 @@ module.exports = class ResearchService extends EventEmitter {
             return;
         }
 
-        let techs = Object.keys(player.research).filter(k => {
-            return k.match(/^[^_\$]/) != null;
-        });
+        let tech = this._getRandomTechnology(game, player);
 
-        techs = techs.filter(t => this.technologyService.isTechnologyEnabled(game, t));
-
-        if (!techs.length) {
+        if (!tech) {
             return;
         }
 
         let effectiveTechs = this.technologyService.getPlayerEffectiveTechnologyLevels(game, player);
-
-        let researchTechsCount = techs.length;
-
-        let techKey = techs[this.randomService.getRandomNumber(researchTechsCount - 1)];
-        let tech = player.research[techKey];
         let researchAmount = effectiveTechs.experimentation * game.constants.research.progressMultiplier;
 
-        tech.progress += researchAmount;
+        tech.technology.progress += researchAmount;
 
         // If the current progress is greater than the required progress
         // then increase the level and carry over the remainder.
-        let requiredProgress = this.getRequiredResearchProgress(game, techKey, tech.level);
+        let requiredProgress = this.getRequiredResearchProgress(game, tech.key, tech.technology.level);
 
         let levelUp = false;
 
-        while (tech.progress >= requiredProgress) {
-            tech.level++;
-            tech.progress -= requiredProgress;
+        while (tech.technology.progress >= requiredProgress) {
+            tech.technology.level++;
+            tech.technology.progress -= requiredProgress;
             levelUp = true;
         }
 
@@ -142,13 +143,35 @@ module.exports = class ResearchService extends EventEmitter {
         let currentResearchTicksEta = this.calculateCurrentResearchETAInTicks(game, player);
 
         return {
-            technology: techKey,
-            level: tech.level,
+            technology: tech.key,
+            level: tech.technology.level,
             // TODO: Return effective tech level
-            progress: tech.progress,
+            progress: tech.technology.progress,
             amount: researchAmount,
             levelUp,
             currentResearchTicksEta
+        };
+    }
+
+    _getRandomTechnology(game, player) {
+        let techs = Object.keys(player.research).filter(k => {
+            return k.match(/^[^_\$]/) != null;
+        });
+
+        techs = techs.filter(t => this.technologyService.isTechnologyEnabled(game, t));
+
+        if (!techs.length) {
+            return null;
+        }
+
+        let researchTechsCount = techs.length;
+
+        let techKey = techs[this.randomService.getRandomNumber(researchTechsCount - 1)];
+        let tech = player.research[techKey];
+
+        return {
+            key: techKey,
+            technology: tech
         };
     }
 
