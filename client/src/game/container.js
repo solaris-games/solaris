1c797981a3033e77f4f26d6f4299c2b9bba90caa
Mike Eason <mike.eason@autointegrate.com>
2020-09-23 16:33:09 +0100

    Increased how far players can zoom out, closes #313

--- client/src/game/container.js
+++ client/src/game/container.js
@@ -55,10 +55,10 @@ class GameContainer {
   setupViewport (game) {
     this.game = game
 
-    this.starFieldLeft = this._calculateMinStarX(game) - 500
-    this.starFieldRight = this._calculateMaxStarX(game) + 500
-    this.starFieldTop = this._calculateMinStarY(game) - 250
-    this.starFieldBottom = this._calculateMaxStarY(game) + 250
+    this.starFieldLeft = this._calculateMinStarX(game) - 1000
+    this.starFieldRight = this._calculateMaxStarX(game) + 1000
+    this.starFieldTop = this._calculateMinStarY(game) - 500
+    this.starFieldBottom = this._calculateMaxStarY(game) + 500
 
     // activate plugins
     this.viewport
