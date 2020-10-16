e5901d6e813a0460414b022844cc2940fac77565
Kurtzmusch <kurtzmusch@gmail.com>
2020-10-16 16:09:31 -0300

    implemented fps counter

--- client/src/game/container.js
+++ client/src/game/container.js
@@ -5,6 +5,21 @@ import Map from './map'
 class GameContainer {
   constructor () {
     PIXI.settings.SORTABLE_CHILDREN = true
+    this.frames = 0
+    this.dtAccum = 0
+  }
+
+  calcFPS(deltaTime) {
+    //assumes PIXI ticker is set to 60(default)
+    this.frames++
+    this.dtAccum += deltaTime/60.0
+    if (this.frames >= 60*5) {
+      let avg = this.dtAccum/(60.0*5.0)
+      console.log( 'avg dt: '+avg )
+      console.log( 'avg fps: '+1000.0/(1000.0*avg) )
+      this.frames = 0
+      this.dtAccum = 0
+    }
   }
 
   setupApp () {
@@ -27,6 +42,8 @@ class GameContainer {
       autoResize: true
     })
     this.app.ticker.add( this.onTick.bind(this) )
+
+    this.app.ticker.add( this.calcFPS.bind(this) )
 
     // create viewport
     this.viewport = new Viewport({
