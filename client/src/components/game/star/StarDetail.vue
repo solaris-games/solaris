5979f54a1ed237bca25c217806d16f9d91ed9ffb
Mike Eason <mike.eason@mail.com>
2021-01-18 22:17:05 +0000

    Fixed toggle bulk upgrade button displaying the wrong colour

--- client/src/components/game/star/StarDetail.vue
+++ client/src/components/game/star/StarDetail.vue
@@ -3,8 +3,8 @@
     <menu-title :title="star.name" @onCloseRequested="onCloseRequested">
       <button @click="toggleBulkIgnore" class="btn" 
         title="Toggle Bulk Ignore"
-        :class="{'btn-danger':!star.ignoreBulkUpgrade,'btn-success':star.ignoreBulkUpgrade}">
-        <i class="fas" :class="{'fa-ban':!star.ignoreBulkUpgrade,'fa-check-square':star.ignoreBulkUpgrade}"></i>
+        :class="{'btn-danger':star.ignoreBulkUpgrade,'btn-success':!star.ignoreBulkUpgrade}">
+        <i class="fas" :class="{'fa-ban':star.ignoreBulkUpgrade,'fa-check-square':!star.ignoreBulkUpgrade}"></i>
       </button>
       <button @click="viewOnMap" class="btn btn-info ml-1"><i class="fas fa-eye"></i></button>
     </menu-title>
