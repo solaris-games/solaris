26057999efcdd1a44b9fdf15387a97d6bc66da29
Mike Eason <mike.eason@mail.com>
2021-01-18 20:34:17 +0000

    Re-index gameevents

--- server/models/schemas/event.js
+++ server/models/schemas/event.js
@@ -3,11 +3,13 @@ const Schema = mongoose.Schema;
 const Types = Schema.Types;
 
 const schema = new Schema({
-    gameId: { type: Types.ObjectId, required: true, index: true },
+    gameId: { type: Types.ObjectId, required: true },
     playerId: { type: Types.ObjectId, required: false },
     tick: { type: Types.Number, required: true },
     type: { type: Types.String, required: true },
     data: { type: Types.Mixed, required: true }
 });
+
+schema.index({gameId: 1, tick: 1}, {unique: false});
 
 module.exports = schema;
