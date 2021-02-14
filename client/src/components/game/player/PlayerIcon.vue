5c9d984fa8979109b5b5507d041ae30a4e660c46
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-02-14 14:23:27 +0100

    Render matching icon shapes

--- client/src/components/game/player/PlayerIcon.vue
+++ client/src/components/game/player/PlayerIcon.vue
@@ -1,7 +1,10 @@
 <template>
   <span v-if="player" class="span-container" :title="onlineStatus">
     <svg viewBox="0 0 512 512">
-      <ellipse :style="getIconStyle()" cx="256" cy="256" rx="224" ry="224" />
+      <ellipse v-if="player.shape === 'circle'" :style="getIconStyle()" cx="256" cy="256" rx="224" ry="224" />
+      <rect v-if="player.shape === 'square'" :style="getIconStyle()" width="448" height="448" />
+      <polygon v-if="player.shape === 'diamond'" :style="getIconStyle()" points="256 32, 480 256, 256 480, 32 256" />
+      <polygon v-if="player.shape === 'hexagon'" :style="getIconStyle()" points="128,32 0,256 128,480 384,480 512,256 384,32 " transform="matrix(0.87415039,0,0,0.87415039,32.2175,32.2175)" />
     </svg>
   </span>
 </template>
