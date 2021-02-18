a553ff0142ebd6bbca567b6d30f0b84488aa2d4c
Mike Eason <mike.eason@mail.com>
2021-02-18 20:09:26 +0000

    Improve performance of player is in scanning range check

--- server/services/player.js
+++ server/services/player.js
@@ -53,10 +53,15 @@ module.exports = class PlayerService extends EventEmitter {
 
             let playerStars = this.starService.listStarsOwnedByPlayer(game.galaxy.stars, p._id);
 
-            let isInRange = playerStars.find(s => {
-                return this.starService.isStarInScanningRangeOfPlayer(game, s, player);       
-            });
+            let isInRange = false;
 
+            for (let s of playerStars) {
+                if (this.starService.isStarInScanningRangeOfPlayer(game, s, player)) {
+                    isInRange = true;
+                    break;
+                }
+            }
+            
             if (isInRange) {
                 inRange.push(p);
             }
@@ -66,7 +71,6 @@ module.exports = class PlayerService extends EventEmitter {
     }
 
     isInScanningRangeOfPlayer(game, sourcePlayer, targetPlayer) {
-        // TODO: Make this more efficient.
         return this.getPlayersWithinScanningRangeOfPlayer(game, sourcePlayer)
             .find(p => p._id.equals(targetPlayer._id)) != null;
     }
