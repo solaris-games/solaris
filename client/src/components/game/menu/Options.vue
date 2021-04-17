fc4b209060b2be90c5a6aa8d8d3c76a16d71bd4e
Mike Eason <mike.eason@mail.com>
2021-04-17 07:58:36 +0000

    Wrap antialiasing into a setting

--- client/src/components/game/menu/Options.vue
+++ client/src/components/game/menu/Options.vue
@@ -153,6 +153,16 @@
           </select>
         </div>
       </div>
+      
+      <div class="row bg-secondary pt-1 pb-1">
+        <label for="antiAliasing" class="col-12 col-sm-6 col-form-label">Anti Aliasing</label>
+        <div class="col-12 col-sm-6">
+          <select class="form-control" id="antiAliasing" v-model="settings.map.antiAliasing" :disabled="isSavingSettings">
+            <option value="enabled">Enabled</option>
+            <option value="disabled">Disabled</option>
+          </select>
+        </div>
+      </div>
 
       <h5 class="pt-2">Carriers</h5>
 
