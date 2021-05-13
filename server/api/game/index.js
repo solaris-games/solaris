3e05bd4ee7c29f04562bfa3956c264338dde727b
Mike Eason <mike.eason@mail.com>
2021-05-13 19:37:20 +0000

    Made get player user API endpoint public

--- server/api/game/index.js
+++ server/api/game/index.js
@@ -231,7 +231,7 @@ module.exports = (router, io, container) => {
         }
     }, middleware.handleError);
 
-    router.get('/api/game/:gameId/player/:playerId', middleware.authenticate, middleware.loadGamePlayers, async (req, res, next) => {
+    router.get('/api/game/:gameId/player/:playerId', middleware.loadGamePlayers, async (req, res, next) => {
         try {
             let user = await container.gameService.getPlayerUserLean(
                 req.game,
