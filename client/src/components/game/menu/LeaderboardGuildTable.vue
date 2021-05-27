1e27ad8f263aee35cf435562a97f3f9c621aabdf
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-05-27 23:22:57 +0200

    Add sorting callbacks and indicators to sortable leaderboard

--- client/src/components/game/menu/LeaderboardGuildTable.vue
+++ client/src/components/game/menu/LeaderboardGuildTable.vue
@@ -1,34 +1,56 @@
 <template>
-  <sortable-leaderboard class="mt-2" :leaderboard="leaderboard" defaultSortingKey="totalRank" @sortingRequested="sortLeaderboard">
-    <template v-slot:header="actions">
-      <th>#</th>
-      <th>Guild</th>
-      <th class="text-right" title="Members" @click="actions.sort('memberCount')"><i class="fas fa-user text-info"></i></th>
-      <th class="text-right" title="Rank" @click="actions.sort('totalRank')"><i class="fas fa-star text-info"></i></th>
-    </template>
-    <template v-slot:row="guild">
-        <td>{{guild.position}}</td>
-        <td>
-            {{guild.name}} [{{guild.tag}}]
-        </td>
-        <td align="right">{{guild.memberCount}}</td>
-        <td align="right">{{guild.totalRank}}</td>
-    </template>
-  </sortable-leaderboard>
+  <div>
+    <loading-spinner :loading="isLoading"/>
+
+    <sortable-leaderboard class="mt-2" :leaderboard="leaderboard" :sortingKey="sortingKey" @sortingRequested="sortLeaderboard" v-if="!isLoading">
+      <template v-slot:header="actions">
+        <th>#</th>
+        <th>Guild</th>
+        <th class="text-right" title="Members" @click="actions.sort('memberCount')" :class="actions.getColumnClass('memberCount')">
+          <i class="fas fa-user text-info"></i>
+          <i v-if="actions.isActive('memberCount')" class="fas fa-chevron-down ml-2"></i>
+        </th>
+        <th class="text-right" title="Rank" @click="actions.sort('totalRank')" :class="actions.getColumnClass('totalRank')">
+          <i class="fas fa-star text-info"></i>
+          <i v-if="actions.isActive('totalRank')" class="fas fa-chevron-down ml-2"></i>
+        </th>
+      </template>
+      <template v-slot:row="guild">
+          <td>{{guild.position}}</td>
+          <td>
+              {{guild.name}} [{{guild.tag}}]
+          </td>
+          <td align="right">{{guild.memberCount}}</td>
+          <td align="right">{{guild.totalRank}}</td>
+      </template>
+    </sortable-leaderboard>
+  </div>
 </template>
 
 <script>
+import LoadingSpinner from '../../LoadingSpinner';
 import SortableLeaderboard from './SortableLeaderboard'
+
 export default {
   components: {
+    'loading-spinner': LoadingSpinner,
     'sortable-leaderboard': SortableLeaderboard
   },
   props: {
     leaderboard: Array
   },
+  data () {
+    return {
+      isLoading: false,
+      sortingKey: 'totalRank'
+    }
+  },
   methods: {
     sortLeaderboard (key) {
+      this.isLoading = true;
       console.log("Sort: " + key);
+      this.sortingKey = key;
+      this.isLoading = false;
     }
   }
 }
--- client/src/components/game/menu/SortableLeaderboard.vue
+++ client/src/components/game/menu/SortableLeaderboard.vue
@@ -2,7 +2,7 @@
   <div class="table-responsive">
     <table class="table table-striped table-hover" v-if="leaderboard">
       <thead>
-        <slot name="header" v-bind:sort="sortBy"></slot>
+        <slot name="header" v-bind:sort="sortBy" v-bind:getColumnClass="getColumnClass" v-bind:isActive="isActiveSorting"></slot>
       </thead>
       <tbody>
         <tr v-for="value in leaderboard" :key="value._id">
@@ -16,17 +16,18 @@
 export default {
   components: {},
   props: {
-    defaultSortingKey: String,
+    sortingKey: String,
     leaderboard: Array,
-  },
-  data() {
-    return {
-      sortingKey: this.defaultSortingKey,
-    };
   },
   methods: {
     sortBy (key) {
       this.$emit('sortingRequested', key)
+    },
+    getColumnClass (colKey) {
+      return { 'table-primary': this.sortingKey === colKey }
+    },
+    isActiveSorting (colKey) {
+      return this.sortingKey === colKey;
     }
   }
 };
