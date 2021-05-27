30c8292f3f33e8764855ee9acbb9dd4740b52d2d
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-05-27 23:37:51 +0200

    Perform guild leaderboard loading inside component

--- client/src/components/game/menu/LeaderboardGuildTable.vue
+++ client/src/components/game/menu/LeaderboardGuildTable.vue
@@ -1,49 +1,68 @@
 <template>
-  <div>
+  <div class="tab-pane fade" id="guilds">
     <loading-spinner :loading="isLoading"/>
+    <div id="guilds" >
 
-    <sortable-leaderboard class="mt-2" :leaderboard="leaderboard" :sortingKey="sortingKey" @sortingRequested="sortLeaderboard" v-if="!isLoading">
-      <template v-slot:header="actions">
-        <th style="width: 10%">#</th>
-        <th style="width: 50%">Guild</th>
-        <th style="width: 20%" class="text-right" title="Members" @click="actions.sort('memberCount')" :class="actions.getColumnClass('memberCount')">
-          <i class="fas fa-user text-info"></i>
-          <i v-if="actions.isActive('memberCount')" class="fas fa-chevron-down ml-2"></i>
-        </th>
-        <th style="width: 20%" class="text-right" title="Rank" @click="actions.sort('totalRank')" :class="actions.getColumnClass('totalRank')">
-          <i class="fas fa-star text-info"></i>
-          <i v-if="actions.isActive('totalRank')" class="fas fa-chevron-down ml-2"></i>
-        </th>
-      </template>
-      <template v-slot:row="guild">
-          <td>{{guild.position}}</td>
-          <td>
-              {{guild.name}} [{{guild.tag}}]
-          </td>
-          <td align="right">{{guild.memberCount}}</td>
-          <td align="right">{{guild.totalRank}}</td>
-      </template>
-    </sortable-leaderboard>
+      <h4 class="mb-1">Top 100 Guilds</h4>
+      <small class="text-warning">Total Guilds: {{totalGuilds}}</small>
+
+      <sortable-leaderboard v-if="leaderboard && !isLoading" class="mt-2" :leaderboard="leaderboard" :sortingKey="sortingKey" @sortingRequested="sortLeaderboard">
+        <template v-slot:header="actions">
+          <th style="width: 10%">#</th>
+          <th style="width: 50%">Guild</th>
+          <th style="width: 20%" class="text-right" title="Members" @click="actions.sort('memberCount')" :class="actions.getColumnClass('memberCount')">
+            <i class="fas fa-user text-info"></i>
+            <i v-if="actions.isActive('memberCount')" class="fas fa-chevron-down ml-2"></i>
+          </th>
+          <th style="width: 20%" class="text-right" title="Rank" @click="actions.sort('totalRank')" :class="actions.getColumnClass('totalRank')">
+            <i class="fas fa-star text-info"></i>
+            <i v-if="actions.isActive('totalRank')" class="fas fa-chevron-down ml-2"></i>
+          </th>
+        </template>
+        <template v-slot:row="guild">
+            <td>{{guild.position}}</td>
+            <td>
+                {{guild.name}} [{{guild.tag}}]
+            </td>
+            <td align="right">{{guild.memberCount}}</td>
+            <td align="right">{{guild.totalRank}}</td>
+        </template>
+      </sortable-leaderboard>
+    </div>
   </div>
 </template>
 
 <script>
 import LoadingSpinner from '../../LoadingSpinner';
-import SortableLeaderboard from './SortableLeaderboard'
+import SortableLeaderboard from './SortableLeaderboard';
+import GuildApiService from '../../../services/api/guild';
 
 export default {
   components: {
     'loading-spinner': LoadingSpinner,
     'sortable-leaderboard': SortableLeaderboard
   },
-  props: {
-    leaderboard: Array
-  },
   data () {
     return {
+      leaderboard: [],
       isLoading: false,
-      sortingKey: 'totalRank'
+      sortingKey: 'totalRank',
+      totalGuilds: 0
     }
+  },
+  async mounted () {
+    this.loading = true;
+    try {
+      const response = await GuildApiService.getLeaderboard(100);
+      if (response.status === 200) {
+        this.leaderboard = response.data.leaderboard;
+        this.totalGuilds = response.data.totalGuilds;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+
+    this.loading = false;
   },
   methods: {
     sortLeaderboard (key) {
--- client/src/views/Leaderboard.vue
+++ client/src/views/Leaderboard.vue
@@ -21,13 +21,7 @@
           <leaderboard-user-table class="mt-2" :leaderboard="currentLeaderboard" :activeSortingKey="userSortingKey" @sortingRequested="switchToLeaderboard"></leaderboard-user-table>
         </div>
       </div>
-      <div class="tab-pane fade" id="guilds">
-        <div v-if="guildLeaderboard">
-          <h4 class="mb-1">Top 10 Guilds</h4>
-          <small class="text-warning">Total Guilds: {{totalGuilds}}</small>
-          <leaderboard-guild-table :leaderboard="guildLeaderboard"></leaderboard-guild-table>
-        </div>
-      </div>
+      <leaderboard-guild-table></leaderboard-guild-table>
     </div>
 
   </view-container>
@@ -38,7 +32,6 @@ import ViewContainer from '../components/ViewContainer'
 import ViewTitle from '../components/ViewTitle'
 import LoadingSpinner from '../components/LoadingSpinner'
 import UserApiService from '../services/api/user'
-import GuildApiService from '../services/api/guild'
 import LeaderboardUserTable from '../components/game/menu/LeaderboardUserTable'
 import LeaderboardGuildTable from '../components/game/menu/LeaderboardGuildTable'
 
@@ -55,19 +48,15 @@ export default {
       isLoading: false,
       userLeaderboards: {},
       userSortingKey: 'rank',
-      totalPlayers: 0,
-      guildLeaderboard: null,
-      totalGuilds: 0
+      totalPlayers: 0
     }
   },
   async mounted () {
     this.isLoading = true
-    this.guildLeaderboard = null
 
     try {
       let requests = [
         await UserApiService.getLeaderboard(100, this.userSortingKey),
-        GuildApiService.getLeaderboard(100)
       ]
 
       let responses = await Promise.all(requests)
@@ -77,11 +66,6 @@ export default {
         this.userLeaderboards = result.leaderboard
         this.$set(this.userLeaderboards, this.userSortingKey, result.leaderboard)
         this.totalPlayers = result.totalPlayers
-      }
-      
-      if (responses[1].status === 200) {
-        this.guildLeaderboard = responses[1].data.leaderboard
-        this.totalGuilds = responses[1].data.totalGuilds
       }
     } catch (err) {
       console.error(err)
