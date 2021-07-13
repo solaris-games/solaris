409e6f308edd6916c9940c7617fe35a95320a306
Mike Eason <mike.eason@mail.com>
2021-07-13 18:47:33 +0000

    UI tweaks to footer

--- client/src/components/ViewContainer.vue
+++ client/src/components/ViewContainer.vue
@@ -7,31 +7,23 @@
         <footer class="mt-3">
           <div class="row">
             <div class="col">
-                <a href="https://github.com/mike-eason/solaris" target="_blank" title="Source Code">
+                <a href="https://github.com/mike-eason/solaris" target="_blank" title="Code" class="mr-2">
                   <i class="fab fa-github"></i>
-                  <span class="d-none d-md-inline-block ml-1">Source Code</span>
                 </a>
-                |
-                <a href="https://store.steampowered.com/app/1623930/Solaris/" target="_blank" title="Steam">
+                <a href="https://store.steampowered.com/app/1623930/Solaris/" target="_blank" title="Steam" class="mr-2">
                   <i class="fab fa-steam"></i>
-                  <span class="d-none d-md-inline-block ml-1">Steam</span>
                 </a>
-                |
-                <a href="https://discord.com/invite/v7PD33d" target="_blank" title="Discord">
+                <a href="https://discord.com/invite/v7PD33d" target="_blank" title="Discord" class="mr-2">
                   <i class="fab fa-discord"></i>
-                  <span class="d-none d-md-inline-block ml-1">Discord</span>
                 </a>
-                |
-                <a href="https://forum.solaris.games" target="_blank" title="Forum">
+                <a href="https://forum.solaris.games" target="_blank" title="Forum" class="mr-2">
                   <i class="far fa-comments"></i>
-                  <span class="d-none d-md-inline-block ml-1">Forum</span>
                 </a>
             </div>
             <div class="col-auto">
-                <a href="https://www.buymeacoffee.com/hyperi0n" target="_blank" class="text-warning">
-                  <i class="fas fa-coffee"></i>
-                  Donate
-                </a>
+                <a href="https://www.redbubble.com/shop/ap/82527983" target="_blank" class="text-info"><i class="fas fa-tshirt mr-1"></i>Shop</a>
+                |
+                <a href="https://www.buymeacoffee.com/hyperi0n" target="_blank" class="text-warning"><i class="fas fa-coffee mr-1"></i>Donate</a>
             </div>
           </div>
         </footer>
