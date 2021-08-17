9d3c8e4255ab75e118fb23ba3b3a3f366adc64b9
Mike Eason <mike.eason@mail.com>
2021-08-17 20:51:07 +0000

    Update wording on gift carrier popups

--- client/src/components/game/carrier/GiftCarrier.vue
+++ client/src/components/game/carrier/GiftCarrier.vue
@@ -39,7 +39,7 @@ export default {
   },
   methods: {
     async giftCarrier (e) {
-      if (!await this.$confirm('Gift carrier', `Are you sure you want to convert ${this.carrier.name} into a gift? If the carrier has a specialist, it will be retired.`)) {
+      if (!await this.$confirm('Gift carrier', `Are you sure you want to convert ${this.carrier.name} into a gift? If the carrier has a specialist, it will be retired when it arrives at the destination.`)) {
         return
       }
 
