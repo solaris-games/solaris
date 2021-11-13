c908ea15c9b55ca177b44c2ec46618226fcfaecc
Mike Eason <mike.eason@mail.com>
2021-11-13 08:33:28 +0000

    Bodge to temporarily fix research UI

--- client/src/components/game/player/Research.vue
+++ client/src/components/game/player/Research.vue
@@ -16,7 +16,8 @@
               v-if="isTechnologyEnabled('scanning')"
               research="scanning"
               iconClass="fa-binoculars"
-              title="Scanning - Determines how far your star's can see"
+              title="Scaning"
+              subtitle="Determines how far your star's can see"
               :player="player"
               :userPlayer="userPlayer"
             ></research-row>
@@ -24,7 +25,8 @@
               v-if="isTechnologyEnabled('hyperspace')"
               research="hyperspace"
               iconClass="fa-gas-pump"
-              title="Hyperspace Range - Determines how far your carriers can travel in a single jump"
+              title="Hyperspace Range"
+              subtitle="Determines how far your carriers can travel in a single jump"
               :player="player"
               :userPlayer="userPlayer"
             ></research-row>
@@ -32,7 +34,8 @@
               v-if="isTechnologyEnabled('terraforming')"
               research="terraforming"
               iconClass="fa-globe-europe"
-              title="Terraforming - Determines infrastructure cost. The higher the terraforming level, the lower infrastructure will cost to upgrade"
+              title="Terraforming"
+              subtitle="Determines infrastructure cost. The higher the terraforming level, the lower infrastructure will cost to upgrade"
               :player="player"
               :userPlayer="userPlayer"
             ></research-row>
@@ -40,7 +43,8 @@
               v-if="isTechnologyEnabled('experimentation')"
               research="experimentation"
               iconClass="fa-microscope"
-              title="Experimentation - Determines how many research points are awarded at the end of the galactic cycle to a random technology"
+              title="Experimentation"
+              subtitle="Determines how many research points are awarded at the end of the galactic cycle to a random technology"
               :player="player"
               :userPlayer="userPlayer"
             ></research-row>
@@ -48,7 +52,8 @@
               v-if="isTechnologyEnabled('weapons')"
               research="weapons"
               iconClass="fa-fighter-jet"
-              title="Weapons - Determines combat strength of your ships"
+              title="Weapons"
+              subtitle="Determines combat strength of your ships"
               :player="player"
               :userPlayer="userPlayer"
             ></research-row>
@@ -56,7 +61,8 @@
               v-if="isTechnologyEnabled('banking')"
               research="banking"
               iconClass="fa-money-bill-alt"
-              title="Banking - Determines how many credits are awarded at the end of the galactic cycle"
+              title="Banking"
+              subtitle="Determines how many credits are awarded at the end of the galactic cycle"
               :player="player"
               :userPlayer="userPlayer"
             ></research-row>
@@ -64,7 +70,8 @@
               v-if="isTechnologyEnabled('manufacturing')"
               research="manufacturing"
               iconClass="fa-industry"
-              title="Manufacturing - Determines how many ships are built via industrial infrastructure per tick"
+              title="Manufacturing"
+              subtitle="Determines how many ships are built via industrial infrastructure per tick"
               :player="player"
               :userPlayer="userPlayer"
             ></research-row>
@@ -72,7 +79,8 @@
               v-if="isTechnologyEnabled('specialists')"
               research="specialists"
               iconClass="fa-user-astronaut"
-              title="Specialists - Determines how many specialist tokens are awarded at the end of the galactic cycle"
+              title="Specialists"
+              subtitle="Determines how many specialist tokens are awarded at the end of the galactic cycle"
               :player="player"
               :userPlayer="userPlayer"
             ></research-row>
