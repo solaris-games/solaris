ea24dd85fc0ba041382a97a7cc0460591c72806f
ScorpioneOrzion <36512390+ScorpioneOrzion@users.noreply.github.com>
2021-12-19 16:30:25 +0100

    updated schema waypoint

--- server/models/schemas/waypoint.js
+++ server/models/schemas/waypoint.js
@@ -4,6 +4,7 @@ const Types = Schema.Types;
 
 const schema = new Schema({
     source: { type: Types.ObjectId, required: true },
+    isCarrier: { type: Types.Boolean, required: true, default: false },
     destination: { type: Types.ObjectId, required: true },
     action: { type: Types.String, required: true, enum: ['nothing', 'collectAll', 'dropAll', 'collect', 'drop', 'collectAllBut', 'dropAllBut', 'dropPercentage', 'collectPercentage', 'garrison'], default: 'nothing' },
     actionShips: { type: Types.Number, required: true, default: 0 },
