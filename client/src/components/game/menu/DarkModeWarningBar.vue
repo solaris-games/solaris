90fd0382131f7f3aaa9b03474504659b53439c38
Mike Eason <mike.eason@mail.com>
2021-12-27 21:30:30 +0000

    Fixed dark mode warning bar not visible on mobile

--- client/src/components/game/menu/DarkModeWarningBar.vue
+++ client/src/components/game/menu/DarkModeWarningBar.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="bg-warning container">
     <div class="row pt-2 pb-2">
-        <div class="col d-none d-sm-block">
+        <div class="col">
             <p class="mt-1 mb-0">You are spectating a dark mode game, no stars will be visible.</p>
         </div>
     </div>
