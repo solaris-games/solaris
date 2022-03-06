0d44ba085d43c703ca1b29624eece0fa59876d2b
Mike Eason <mike.eason@mail.com>
2022-03-06 21:37:43 +0000

    Restyled global events

--- client/src/components/game/eventLog/events/EventsListItem.vue
+++ client/src/components/game/eventLog/events/EventsListItem.vue
@@ -1,11 +1,12 @@
 <template>
-<div class="bg-primary pt-2 pb-1 mb-2 pointer" :class="{'unread':!isRead}" @click="markAsRead">
-    <div v-if="event.tick">
-        <div class="col text-right">
-            <span class="badge" :class="{'badge-success':isRead,'badge-warning':!isRead}">Tick {{event.tick}}</span>
-        </div>
+<div class="bg-primary pt-2 pb-1 mb-2 pointer row" :class="{'unread':!isRead, 'bg-secondary': isGlobal}" @click="markAsRead">
+    <div class="col">
+        <span class="badge badge-info" v-if="isGlobal">Global Event</span>
     </div>
-    <div class="col mt-2">
+    <div v-if="event.tick" class="col-auto">
+        <span class="badge" :class="{'badge-success':isRead,'badge-warning':!isRead}">Tick {{event.tick}}</span>
+    </div>
+    <div class="col-12 mt-2">
         <game-ended :event="event" v-if="event.type === 'gameEnded'"
             @onOpenPlayerDetailRequested="onOpenPlayerDetailRequested"/>
         <game-paused :event="event" v-if="event.type === 'gamePaused'"/>
@@ -164,6 +165,9 @@ export default {
   computed: {
       isRead () {
           return this.event.read || this.event.read == null
+      },
+      isGlobal () {
+          return this.event.read == null
       }
   }
 }
@@ -175,6 +179,7 @@ export default {
 }
 
 .unread {
-    border: 3px solid #f39c12;
+    border-top: 4px solid #f39c12;
+    border-bottom: 4px solid #f39c12;
 }
 </style>
