bac96a6be97f811de525a13c924cddcb2705a98e
Mike Eason <mike.eason@mail.com>
2022-04-29 20:43:54 +0000

    UI tweak

--- client/src/views/game/components/player/PlayerOpenSlotStatus.vue
+++ client/src/views/game/components/player/PlayerOpenSlotStatus.vue
@@ -1,8 +1,8 @@
 <template>
-<div v-if="player && player.isOpenSlot" class="row bg-info">
+<div v-if="player && player.isOpenSlot" class="row bg-warning">
   <div class="col pt-1 pb-1 mt-0 mb-0">
     <p class="mb-0 mt-0">
-      <small><i class="fas fa-user-gear"></i> This player slot is open.</small>
+      <small><i class="fas fa-user-clock"></i> This player slot is open.</small>
     </p>
   </div>
 </div>
