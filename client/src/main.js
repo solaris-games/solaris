7248d683d9a1c516fce31a87db84f768f2d7e942
Mike Eason <mike.eason@mail.com>
2022-06-14 19:25:16 +0000

    Fixed Steam client not opening modals

--- client/src/main.js
+++ client/src/main.js
@@ -10,6 +10,13 @@ import $ from 'jquery'
 import 'pixi.js-legacy'
 import 'pixi-viewport'
 
+// Note: This was done to get around an issue where the Steam client
+// had bootstrap as undefined. This also affects the UI template we're using,
+// we are forced to bring in Bootstrap and FontAwesome manually as a dependency 
+// instead of using the vendor files provided by the template.
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+require('../public/assets/js/app.min.js')
+
 Vue.config.productionTip = false
 window.$ = $;
 
