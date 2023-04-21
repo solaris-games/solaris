d1dcd722d255fa53f44f1169b5a483ea8693a34c
Mike Eason <mike.eason@mail.com>
2023-04-21 08:10:55 +0000

    Immortalise the GameMonth game for youtube series

--- server/jobs/cleanupOldGameHistory.ts
+++ server/jobs/cleanupOldGameHistory.ts
@@ -15,6 +15,12 @@ export default (container: DependencyContainer) => {
             for (let i = 0; i < games.length; i++) {
                 let game = games[i];
 
+                // TODO: This is a quick bodge to immortalise this game: https://solaris.games/#/game?id=63bcb3de8b616b3bbe0e084b
+                // TODO: Add a new `immortal` property to games so that they are ignored by this cleanup job.
+                if (game._id.toString() === '63bcb3de8b616b3bbe0e084b') {
+                    continue
+                }
+
                 console.log(`Deleting history for old game: ${game._id}`);
 
                 try {
