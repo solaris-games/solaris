6aeec0d828abdbdf50884dd14a5f93255730819c
Mike Eason <mike.eason@mail.com>
2024-01-13 16:28:39 +0000

    Fixed convo exploit part 2

--- server/services/conversation.ts
+++ server/services/conversation.ts
@@ -247,7 +247,7 @@ export default class ConversationService extends EventEmitter {
             throw new ValidationError(`The conversation requested does not exist.`);
         }
 
-        if (convo.participants.find(p => p.toString() === player._id.toString() == null)) {
+        if (convo.participants.find(p => p.toString() === player._id.toString()) == null) {
             throw new ValidationError(`You are not participating in this conversation.`);
         }
 
