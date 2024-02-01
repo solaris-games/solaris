e3fd263dcbd9bcd65d9fe4badd2c595af865ebf0
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2024-02-01 22:44:23 +0100

    Implement sequential rotation

--- server/jobs/officialGamesCheck.ts
+++ server/jobs/officialGamesCheck.ts
@@ -1,5 +1,5 @@
 import {DependencyContainer} from "../services/types/DependencyContainer";
-import {GameSettings} from "../services/types/Game";
+import {Game, GameSettings} from "../services/types/Game";
 import {OfficialGameCategory, OfficialGameKind} from "../config/officialGames";
 
 const chooseSetting = (container: DependencyContainer, category: OfficialGameCategory): GameSettings => {
@@ -12,29 +12,45 @@ const chooseSetting = (container: DependencyContainer, category: OfficialGameCat
     }
 }
 
+const findExistingGame = (setting: OfficialGameCategory, games: Game[]) => {
+    if (setting.kind === OfficialGameKind.Standard) {
+        return games.find(x => x.settings.general.type === setting.settings.general.type);
+    } else if (setting.kind === OfficialGameKind.Carousel) {
+        const types = setting.rotation.map(x => x.general.type);
+        return games.find(x => types.includes(x.settings.general.type));
+    }
+}
+
 export default (container: DependencyContainer) => {
     return {
         async handler(job, done) {
             // Check if there is an official game with the settings game name which
             // is currently waiting for players.
-            const games = await container.gameListService.listOfficialGames();
+            const openGames = await container.gameListService.listOfficialGames();
+            const runningGames = await container.gameListService.listInProgressGames();
 
             const settings = container.gameTypeService.getOfficialGameSettings();
 
-            for (const setting of settings) {
-                let existing;
-                if (setting.kind === OfficialGameKind.Standard) {
-                    existing = games.find(x => x.settings.general.type === setting.settings.general.type);
-                } else if (setting.kind === OfficialGameKind.Carousel) {
-                    const types = setting.rotation.map(x => x.general.type);
-                    existing = games.find(x => types.includes(x.settings.general.type));
-                }
+            for (const category of settings) {
+                const existingOpen = findExistingGame(category, openGames);
 
-                if (!existing) {
-                    console.log(`Could not find game [${container.gameTypeService.getOfficialGameCategoryName(setting)}], creating it now...`);
+                if (!existingOpen) {
+                    console.log(`Could not find game [${container.gameTypeService.getOfficialGameCategoryName(category)}], creating it now...`);
+
+                    let setting;
+                    const existingRunning = findExistingGame(setting, runningGames);
+                    const existingTemplate = existingRunning?.settings.general.createdFromTemplate;
+                    
+                    if (existingRunning && existingTemplate && category.kind === OfficialGameKind.Carousel && category.distribution === 'sequential') {
+                        const index = category.rotation.findIndex(x => x.general.createdFromTemplate && x.general.createdFromTemplate === existingTemplate);
+                        const nextIndex = (index + 1) % category.rotation.length;
+                        setting = category.rotation[nextIndex];
+                    } else {
+                        setting = chooseSetting(container, setting);
+                    }
 
                     try {
-                        const newGame = await container.gameCreateService.create(chooseSetting(container, setting));
+                        const newGame = await container.gameCreateService.create(setting);
 
                         console.log(`[${newGame.settings.general.name}] game created.`);
                     } catch (e) {
