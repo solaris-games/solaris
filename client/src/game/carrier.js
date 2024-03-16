82655769447ca38df9eb2480e9ca036f9e51f13a
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2024-03-16 01:15:12 +0100

    Fix pixi interaction issues

--- client/src/game/carrier.js
+++ client/src/game/carrier.js
@@ -12,9 +12,9 @@ class Carrier extends EventEmitter {
   constructor ( pathManager ) {
     super()
 
-    this.container = new PIXI.Container()
     this.fixedContainer = new PIXI.Container() // this container isnt affected by culling or user setting scalling
-    this.container.interactive = true
+    this.container = new PIXI.Container()
+    this.container.eventMode = 'static'
     this.container.interactiveChildren = false
     this.container.buttonMode = true
 
@@ -101,7 +101,7 @@ class Carrier extends EventEmitter {
       this.container.removeChild(this.graphics_colour)
       this.graphics_colour = null
     }
-    
+
     if (!this.data.orbiting) {
       this.drawShape();
     }
@@ -272,16 +272,16 @@ class Carrier extends EventEmitter {
   enableInteractivity() {
     // Can only be interactive if its in transit
     if (!this.data.orbiting) {
-      this.container.interactive = true
+      this.container.eventMode = 'static'
       this.container.buttonMode = true
     } else {
-      this.container.interactive = false
+      this.container.eventMode = 'passive'
       this.container.buttonMode = false
     }
   }
 
   disableInteractivity() {
-    this.container.interactive = false
+    this.container.eventMode = 'passive'
     this.container.buttonMode = false
   }
 
