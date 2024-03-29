8c25f0ec7b38617cb06e0c1590bfece2dc9f7990
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2024-03-29 23:45:09 +0100

    Do not show donation modal in tutorials

--- client/src/views/game/components/GameContainer.vue
+++ client/src/views/game/components/GameContainer.vue
@@ -6,6 +6,7 @@
 import { mapState } from 'vuex'
 import GameContainer from '../../../game/container'
 import GameApiService from '../../../services/api/game'
+import GameHelper from "@/services/gameHelper";
 
 export default {
   data () {
@@ -30,10 +31,12 @@ export default {
   },
 
   mounted () {
+    const game = this.$store.state.game;
+
     // Add the game canvas to the screen.
     this.$el.appendChild(this.gameContainer.app.view) // Add the pixi canvas to the element.
 
-    this.drawGame(this.$store.state.game)
+    this.drawGame(game)
 
     // Bind to game events.
     this.onStarClickedHandler = this.onStarClicked.bind(this)
@@ -55,7 +58,7 @@ export default {
       this.touchPlayer()
     }
 
-    this.tryShowDonateModal()
+    this.tryShowDonateModal(game)
   },
 
   beforeDestroy () {
@@ -116,7 +119,11 @@ export default {
     onObjectsClicked (e) {
       this.$emit('onObjectsClicked', e)
     },
-    async tryShowDonateModal () {
+    async tryShowDonateModal (game) {
+      if (GameHelper.isTutorialGame(game)) {
+        return;
+      }
+
       let chance = Math.floor(Math.random() * (20 - 0 + 1) + 0); // 1 in 20
 
       if (chance === 0 &&
