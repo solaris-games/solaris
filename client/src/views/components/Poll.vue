fddcef5799e94a2c4f99d2060b12b1cd00d328f7
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2024-04-27 20:07:44 +0200

    Keep poll teaser

--- client/src/views/components/Poll.vue
+++ client/src/views/components/Poll.vue
@@ -1,33 +1,17 @@
 <template>
   <div v-if="displayPoll" class="alert alert-success fade show poll">
     <span>Please participate in the 2024 <a class="alert-link" target="_blank" href="https://docs.google.com/forms/d/1HjfAphQ9EG8rRla_ipTIJAudbGZEoOwinCdqTkHcYb8">Solaris Community Poll!</a></span>
-    <button type="button" class="btn btn-secondary btn-sm" @click="dismissPoll" aria-label="Close">Do not ask again</button>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      displayPoll: false
-    }
-  },
-  mounted () {
-    this.loadState();
-  },
-  methods: {
-    loadState () {
-      const item = localStorage.getItem('solaris-poll-2024');
-      this.displayPoll = !item || item !== 'dismissed'
-    },
-    saveState () {
-      if (!this.displayPoll) {
-        localStorage.setItem('solaris-poll-2024', 'dismissed')
-      }
-    },
-    async dismissPoll () {
-      this.displayPoll = false
-      this.saveState()
+  computed: {
+    displayPoll () {
+      const now = new Date();
+      const endsAt = new Date('2024-05-15T00:00:00Z');
+
+      return now < endsAt;
     }
   }
 }
