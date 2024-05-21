711f215b0d02df7eb465bb8c1206ab89618d2cdc
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2024-05-22 01:47:15 +0200

    Format date since not everyone appreciates iso 8601

--- client/src/views/account/Warnings.vue
+++ client/src/views/account/Warnings.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="row pb-0 warnings">
     <div v-for="warning of warnings" class="alert alert-warning fade show">
-      <p>{{ warning.date }}: You have received a warning for: <span class="warningDescription">{{ toDescription(warning.kind) }}</span></p>
+      <p>{{ formatDate(warning.date) }}: You have received a warning for: <span class="warningDescription">{{ toDescription(warning.kind) }}</span></p>
     </div>
   </div>
 </template>
 <script>
+import GameHelper from "../../services/gameHelper";
+
 export default {
   props: {
     warnings: Array
   },
   methods: {
+    formatDate(date) {
+      return GameHelper.getDateString(date)
+    },
     toDescription(warningKind) {
       switch (warningKind) {
         case 'afk':
