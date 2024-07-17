269aad283dbad669dffe186a8a676f46d29bd67b
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2024-07-17 02:34:06 +0200

    Adapt to new route style

--- server/api/routes/announcements.ts
+++ server/api/routes/announcements.ts
@@ -3,30 +3,36 @@ import {MiddlewareContainer} from "../middleware";
 import {ExpressJoiInstance} from "express-joi-validation";
 import {DependencyContainer} from "../../services/types/DependencyContainer";
 import AnnouncementController from "../controllers/announcement";
+import {singleRoute} from "../singleRoute";
 
 export default (router: Router, mw: MiddlewareContainer, validator: ExpressJoiInstance, container: DependencyContainer) => {
     const controller =  AnnouncementController(container);
 
     router.get("/api/announcements/latest",
-        controller.getLatestAnnouncement,
-        mw.core.handleError);
+        ...singleRoute(
+            controller.getLatestAnnouncement,
+            mw.core.handleError));
 
     router.get("/api/announcements/",
-        controller.getAllAnnouncements,
-        mw.core.handleError);
+        ...singleRoute(
+            controller.getAllAnnouncements,
+            mw.core.handleError));
 
     router.get("/api/announcements/unread",
-        mw.auth.authenticate,
-        controller.getUnreadAnnouncements,
-        mw.core.handleError);
+        ...singleRoute(
+            mw.auth.authenticate,
+            controller.getUnreadAnnouncements,
+            mw.core.handleError));
 
     router.get("/api/announcements/state",
-        mw.auth.authenticate(),
-        controller.getAnnouncementState,
-        mw.core.handleError);
+        ...singleRoute(
+            mw.auth.authenticate(),
+            controller.getAnnouncementState,
+            mw.core.handleError));
 
     router.patch("/api/announcements/state/markAsRead",
-        mw.auth.authenticate(),
-        controller.markAsRead,
-        mw.core.handleError)
+        ...singleRoute(
+            mw.auth.authenticate(),
+            controller.markAsRead,
+            mw.core.handleError))
 }
