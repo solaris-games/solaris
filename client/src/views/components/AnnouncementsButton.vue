215505dea48ee9b8c0421610d753520464d598f6
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2024-07-20 23:14:01 +0200

    Add main menu button for announcements

---
+++ client/src/views/components/AnnouncementsButton.vue
@@ -0,0 +1,13 @@
+<template>
+ <button class="btn btn-info"><span class="badge bg-default">2</span> Announcements</button>
+</template>
+
+<script>
+export default {
+  name: "AnnouncementsButton"
+}
+</script>
+
+<style scoped>
+
+</style>
