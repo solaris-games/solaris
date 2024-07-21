df7d8c74cc8c7c7e1363d693f4c184170cf54124
Metritutus <324345+Metritutus@users.noreply.github.com>
2024-07-22 00:40:23 +0100

    * Added idempotent functionality to all unsafe HTTP method requests, to prevent requests like POST, PUT, etc from being actioned multiple times in instances where, say, the client may be retrying a request due to it not receiving feedback from the server due to connection issues (eg poor mobile signal, etc).  This should fix instances where a player may accidentally buy multiple carriers where they only meant to buy one, or accidentally create two conversations with the same player, etc. * Updated carrier gift modal wording to make clear that the specialist will be retired if the destination star does not belong to an ally. * Updated express.ts to remove unnecessary setting of cors Access-Control-Allow-Headers (it was being replaced by the line directly below it anyway).

--- server/api/express.ts
+++ server/api/express.ts
@@ -11,6 +11,8 @@ import registerRoutes from './routes';
 
 export default async (config: Config, app, container: DependencyContainer) => {
 
+    let idempotencyKeyCache: Map<string, number> = new Map<string, number>();
+
     app.use(require('body-parser').json({
         limit: '1000kb' // Note: This allows large custom galaxies to be uploaded.
     }));
@@ -44,11 +46,36 @@ export default async (config: Config, app, container: DependencyContainer) => {
     // Enable CORS
     app.use((req, res, next) => {
         if (config.corsUrls.includes(req.headers.origin)) {
-            res.header("Access-Control-Allow-Origin", req.headers.origin);
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            res.header('Access-Control-Allow-Headers', 'Content-Type');
+            res.header('Access-Control-Allow-Origin', req.headers.origin);
+            res.header('Access-Control-Allow-Headers', 'Content-Type, Idempotency-Key');
             res.header('Access-Control-Allow-Credentials', 'true');
             res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE, OPTIONS');
+        }
+
+        return next();
+    });
+
+    app.use(async (req, res, next) => {
+        let idempotencyKey: string = req.header('idempotency-key')?.trim() ?? '';
+
+        // Eliminate the expired entries.
+        for (let key of idempotencyKeyCache.keys()) {
+
+            let expiryTimestamp: number | undefined = idempotencyKeyCache.get(key);
+
+            if (expiryTimestamp != null && typeof expiryTimestamp === 'number' && Date.now() > expiryTimestamp) {
+                idempotencyKeyCache.delete(key);
+            }
+        }
+
+        if (idempotencyKey !== '') {
+            if (idempotencyKeyCache.get(idempotencyKey) != null) {
+                res.sendStatus(409);
+                return; // Duplicate request, abort!
+            }
+            else {
+                idempotencyKeyCache.set(idempotencyKey, Date.now() + 300000); // Expire these after 5 minutes.
+            }
         }
 
         return next();
