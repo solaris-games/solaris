6bd063439f5af703319398e2f5e94d81e1320f6d
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2024-10-20 12:31:34 +0200

    Add warning comment

--- client/src/main.js
+++ client/src/main.js
@@ -15,6 +15,7 @@ import ToastPlugin from "vue-toast-notification";
 // had bootstrap as undefined. This also affects the UI template we're using,
 // we are forced to bring in Bootstrap and FontAwesome manually as a dependency
 // instead of using the vendor files provided by the template.
+// DO NOT use top-level await since that silently breaks the bundle
 import('bootstrap/dist/js/bootstrap.bundle.js').then((mod) => window.bootstrap = mod);
 import('../public/assets/js/app.min.js')
 
