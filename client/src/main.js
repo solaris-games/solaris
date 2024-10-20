c8ca1c1205bd08bf9a2abdc5537ad5f629b9c815
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2024-10-20 02:45:07 +0200

    Fix prod build being broken by top-level await import

--- client/src/main.js
+++ client/src/main.js
@@ -15,8 +15,8 @@ import ToastPlugin from "vue-toast-notification";
 // had bootstrap as undefined. This also affects the UI template we're using,
 // we are forced to bring in Bootstrap and FontAwesome manually as a dependency
 // instead of using the vendor files provided by the template.
-window.bootstrap = await import('bootstrap/dist/js/bootstrap.bundle.js');
-await import('../public/assets/js/app.min.js')
+import('bootstrap/dist/js/bootstrap.bundle.js').then((mod) => window.bootstrap = mod);
+import('../public/assets/js/app.min.js')
 
 window.$ = $;
 
