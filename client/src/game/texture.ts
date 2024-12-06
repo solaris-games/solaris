6ebe1a9b52cf9ed6c5f2fe9aa37b659af324b90c
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2024-12-07 00:39:24 +0100

    Extract texture URLs to make them loadable later

--- client/src/game/texture.ts
+++ client/src/game/texture.ts
@@ -1,171 +1,201 @@
-import { Texture, TextStyle, BitmapFont } from 'pixi.js'
+import {Texture, TextStyle, BitmapFont} from 'pixi.js'
 import seededRandom from 'random-seed'
+
+const TEXTURE_URLS = {
+  CARRIER: new URL('../assets/map-objects/128x128_carrier.svg', import.meta.url).href,
+  STAR: new URL('../assets/stars/star.png', import.meta.url).href,
+  STARLESS_NEBULA_1: new URL('../assets/nebula/neb0-starless.png', import.meta.url).href,
+  STARLESS_NEBULA_2: new URL('../assets/nebula/neb1-starless.png', import.meta.url).href,
+  STAR_NEBULA_1: new URL('../assets/nebula/star-nebula-0.png', import.meta.url).href,
+  STAR_NEBULA_2: new URL('../assets/nebula/star-nebula-1.png', import.meta.url).href,
+  STAR_NEBULA_3: new URL('../assets/nebula/star-nebula-2.png', import.meta.url).href,
+  STAR_ASTEROID_FIELD_1: new URL('../assets/stars/star-asteroid-field-0.png', import.meta.url).href,
+  STAR_ASTEROID_FIELD_2: new URL('../assets/stars/star-asteroid-field-1.png', import.meta.url).href,
+  STAR_ASTEROID_FIELD_3: new URL('../assets/stars/star-asteroid-field-2.png', import.meta.url).href,
+  STAR_WORMHOLE: new URL('../assets/stars/vortex.png', import.meta.url).href,
+  CIRCLES: [
+    new URL('../assets/map-objects/256x256_circle.svg', import.meta.url).href,
+    new URL('../assets/map-objects/256x256_circle_warp_gate.svg', import.meta.url).href,
+    new URL('../assets/map-objects/256x256_circle_partial.svg', import.meta.url).href,
+    new URL('../assets/map-objects/256x256_circle_partial_warp_gate.svg', import.meta.url).href,
+    new URL('../assets/map-objects/128x128_circle_carrier.svg', import.meta.url).href,
+  ],
+  HEXAGONS: [
+    new URL('../assets/map-objects/256x256_hexagon.svg', import.meta.url).href,
+    new URL('../assets/map-objects/256x256_hexagon_warp_gate.svg', import.meta.url).href,
+    new URL('../assets/map-objects/256x256_hexagon_partial.svg', import.meta.url).href,
+    new URL('../assets/map-objects/256x256_hexagon_partial_warp_gate.svg', import.meta.url).href,
+    new URL('../assets/map-objects/128x128_hexagon_carrier.svg', import.meta.url).href,
+  ],
+  DIAMONDS: [
+    new URL('../assets/map-objects/256x256_diamond.svg', import.meta.url).href,
+    new URL('../assets/map-objects/256x256_diamond_warp_gate.svg', import.meta.url).href,
+    new URL('../assets/map-objects/256x256_diamond_partial.svg', import.meta.url).href,
+    new URL('../assets/map-objects/256x256_diamond_partial_warp_gate.svg', import.meta.url).href,
+    new URL('../assets/map-objects/128x128_diamond_carrier.svg', import.meta.url).href,
+  ],
+  SQUARES: [
+    new URL('../assets/map-objects/256x256_square.svg', import.meta.url).href,
+    new URL('../assets/map-objects/256x256_square_warp_gate.svg', import.meta.url).href,
+    new URL('../assets/map-objects/256x256_square_partial.svg', import.meta.url).href,
+    new URL('../assets/map-objects/256x256_square_partial_warp_gate.svg', import.meta.url).href,
+    new URL('../assets/map-objects/128x128_square_carrier.svg', import.meta.url).href,
+  ],
+  STAR_SCANNABLE: new URL('../assets/map-objects/128x128_star_scannable.svg', import.meta.url).href,
+  STAR_UNSCANNABLE: new URL('../assets/map-objects/128x128_star_unscannable.svg', import.meta.url).href,
+  HOME: new URL('../assets/map-objects/128x128_star_home.svg', import.meta.url).href,
+  BLACK_HOLE: new URL('../assets/map-objects/128x128_star_black_hole.svg', import.meta.url).href,
+  BLACK_HOLE_BINARY: new URL('../assets/map-objects/128x128_star_black_hole_binary.svg', import.meta.url).href,
+  BINARY_SCANNABLE: new URL('../assets/map-objects/128x128_star_scannable_binary.svg', import.meta.url).href,
+  BINARY_UNSCANNABLE: new URL('../assets/map-objects/128x128_star_unscannable_binary.svg', import.meta.url).href
+}
+
+const SPECIALIST_TEXTURES = [
+  'mecha-head',
+  'mecha-mask',
+  'android-mask',
+  'hazmat-suit',
+  'cyborg-face',
+  'lunar-module',
+  'spaceship',
+  'power-generator',
+  'energise',
+  'sattelite',
+  'airtight-hatch',
+  'cannister',
+  'defense-satellite',
+  'habitat-dome',
+  'techno-heart',
+  'missile-pod',
+  'space-suit',
+  'strafe',
+  'ringed-planet',
+  'observatory',
+  'alien-stare',
+  'afterburn',
+  'pirate',
+  'spoutnik',
+  'starfighter',
+  'double-ringed-orb',
+  'rocket',
+  'ray-gun',
+  'radar-dish',
+  'energy-tank',
+  'cryo-chamber',
+  'vintage-robot',
+  'targeting',
+  'rocket-thruster',
+  'megabot',
+  'forward-field',
+  'star-gate',
+  'bolter-gun'
+];
 
 class TextureService {
 
-    static WARP_GATE_INDEX = 1
-    static PARTIAL_STRIDE = 2
-    static seededRNG = seededRandom.create()
+  static WARP_GATE_INDEX = 1
+  static PARTIAL_STRIDE = 2
+  static seededRNG = seededRandom.create()
 
-    STARLESS_NEBULA_TEXTURES: Texture[] = []
-    STAR_NEBULA_TEXTURES: Texture[] = []
-    SPECIALIST_TEXTURES = {}
-    PLAYER_SYMBOLS = {}
-    STAR_SYMBOLS = {}
+  STARLESS_NEBULA_TEXTURES: Texture[] = []
+  STAR_NEBULA_TEXTURES: Texture[] = []
+  SPECIALIST_TEXTURES = {}
+  PLAYER_SYMBOLS = {}
+  STAR_SYMBOLS = {}
 
-    DEFAULT_FONT_STYLE: TextStyle | undefined;
-    DEFAULT_FONT_STYLE_BOLD: TextStyle | undefined;
-    CARRIER_TEXTURE: Texture | undefined;
-    STAR_WORMHOLE_TEXTURES: Texture[] = [];
-    STAR_ASTEROID_FIELD_TEXTURES: Texture[] = [];
-    STAR_TEXTURE: Texture | undefined;
+  DEFAULT_FONT_STYLE: TextStyle | undefined;
+  DEFAULT_FONT_STYLE_BOLD: TextStyle | undefined;
+  CARRIER_TEXTURE: Texture | undefined;
+  STAR_WORMHOLE_TEXTURES: Texture[] = [];
+  STAR_ASTEROID_FIELD_TEXTURES: Texture[] = [];
+  STAR_TEXTURE: Texture | undefined;
 
-    initialize () {
-      this._loadPlayerSymbols()
-      this._loadStarSymbols()
+  initialize() {
+    this._loadPlayerSymbols()
+    this._loadStarSymbols()
 
-      this.CARRIER_TEXTURE = Texture.from(new URL('../assets/map-objects/128x128_carrier.svg', import.meta.url).href)
-      this.DEFAULT_FONT_STYLE = new TextStyle({
-        fontFamily: `Chakra Petch,sans-serif;`,
-        fill: 0xFFFFFF,
-        padding: 3
-      })
+    this.CARRIER_TEXTURE = Texture.from(TEXTURE_URLS.CARRIER)
+    this.DEFAULT_FONT_STYLE = new TextStyle({
+      fontFamily: `Chakra Petch,sans-serif;`,
+      fill: 0xFFFFFF,
+      padding: 3
+    })
 
-      this.DEFAULT_FONT_STYLE_BOLD = new TextStyle({
-        fontFamily: `Chakra Petch,sans-serif;`,
-        fill: 0xFFFFFF,
-        fontWeight: "bold",
-        padding: 3
-      })
+    this.DEFAULT_FONT_STYLE_BOLD = new TextStyle({
+      fontFamily: `Chakra Petch,sans-serif;`,
+      fill: 0xFFFFFF,
+      fontWeight: "bold",
+      padding: 3
+    })
 
-      this.STAR_TEXTURE = Texture.from(new URL('../assets/stars/star.png', import.meta.url).href)
+    this.STAR_TEXTURE = Texture.from(TEXTURE_URLS.STAR)
 
-      // STARLESS NEBULAS
-      this.STARLESS_NEBULA_TEXTURES.push(Texture.from(new URL('../assets/nebula/neb0-starless.png', import.meta.url).href))
-      this.STARLESS_NEBULA_TEXTURES.push(Texture.from(new URL('../assets/nebula/neb1-starless.png', import.meta.url).href))
+    // STARLESS NEBULAS
+    this.STARLESS_NEBULA_TEXTURES.push(Texture.from(TEXTURE_URLS.STARLESS_NEBULA_1))
+    this.STARLESS_NEBULA_TEXTURES.push(Texture.from(TEXTURE_URLS.STARLESS_NEBULA_2))
 
-      // STAR NEBULAS
-      this.STAR_NEBULA_TEXTURES.push(Texture.from(new URL('../assets/nebula/star-nebula-0.png', import.meta.url).href))
-      this.STAR_NEBULA_TEXTURES.push(Texture.from(new URL('../assets/nebula/star-nebula-1.png', import.meta.url).href))
-      this.STAR_NEBULA_TEXTURES.push(Texture.from(new URL('../assets/nebula/star-nebula-2.png', import.meta.url).href))
+    // STAR NEBULAS
+    this.STAR_NEBULA_TEXTURES.push(Texture.from(TEXTURE_URLS.STAR_NEBULA_1))
+    this.STAR_NEBULA_TEXTURES.push(Texture.from(TEXTURE_URLS.STAR_NEBULA_2))
+    this.STAR_NEBULA_TEXTURES.push(Texture.from(TEXTURE_URLS.STAR_NEBULA_3))
 
-      // STAR ASTEROID FIELDS
-      this.STAR_ASTEROID_FIELD_TEXTURES.push(Texture.from(new URL('../assets/stars/star-asteroid-field-0.png', import.meta.url).href))
-      this.STAR_ASTEROID_FIELD_TEXTURES.push(Texture.from(new URL('../assets/stars/star-asteroid-field-1.png', import.meta.url).href))
-      this.STAR_ASTEROID_FIELD_TEXTURES.push(Texture.from(new URL('../assets/stars/star-asteroid-field-2.png', import.meta.url).href))
+    // STAR ASTEROID FIELDS
+    this.STAR_ASTEROID_FIELD_TEXTURES.push(Texture.from(TEXTURE_URLS.STAR_ASTEROID_FIELD_1))
+    this.STAR_ASTEROID_FIELD_TEXTURES.push(Texture.from(TEXTURE_URLS.STAR_ASTEROID_FIELD_2))
+    this.STAR_ASTEROID_FIELD_TEXTURES.push(Texture.from(TEXTURE_URLS.STAR_ASTEROID_FIELD_3))
 
-      this.STAR_WORMHOLE_TEXTURES.push(Texture.from(new URL('../assets/stars/vortex.png', import.meta.url).href))
+    this.STAR_WORMHOLE_TEXTURES.push(Texture.from(TEXTURE_URLS.STAR_WORMHOLE))
 
-      // SPECIALISTS
-      this._loadSpecialistTexture('mecha-head')
-      this._loadSpecialistTexture('mecha-mask')
-      this._loadSpecialistTexture('android-mask')
-      this._loadSpecialistTexture('hazmat-suit')
-      this._loadSpecialistTexture('cyborg-face')
-      this._loadSpecialistTexture('lunar-module')
-      this._loadSpecialistTexture('spaceship')
-      this._loadSpecialistTexture('power-generator')
-      this._loadSpecialistTexture('energise')
-      this._loadSpecialistTexture('sattelite')
-      this._loadSpecialistTexture('airtight-hatch')
-      this._loadSpecialistTexture('cannister')
-      this._loadSpecialistTexture('defense-satellite')
-      this._loadSpecialistTexture('habitat-dome')
-      this._loadSpecialistTexture('techno-heart')
-      this._loadSpecialistTexture('missile-pod')
-      this._loadSpecialistTexture('space-suit')
-      this._loadSpecialistTexture('strafe')
-      this._loadSpecialistTexture('ringed-planet')
-      this._loadSpecialistTexture('observatory')
-      this._loadSpecialistTexture('alien-stare')
-      this._loadSpecialistTexture('afterburn')
-      this._loadSpecialistTexture('pirate')
-      this._loadSpecialistTexture('spoutnik')
-      this._loadSpecialistTexture('starfighter')
-      this._loadSpecialistTexture('double-ringed-orb')
-      this._loadSpecialistTexture('rocket')
-      this._loadSpecialistTexture('ray-gun')
-      this._loadSpecialistTexture('radar-dish')
-      this._loadSpecialistTexture('energy-tank')
-      this._loadSpecialistTexture('cryo-chamber')
-      this._loadSpecialistTexture('vintage-robot')
-      this._loadSpecialistTexture('targeting')
-      this._loadSpecialistTexture('rocket-thruster')
-      this._loadSpecialistTexture('megabot')
-      this._loadSpecialistTexture('forward-field')
-      this._loadSpecialistTexture('star-gate')
-      this._loadSpecialistTexture('bolter-gun')
+    for (let specTex of SPECIALIST_TEXTURES) {
+      this._loadSpecialistTexture(specTex)
     }
+  }
 
-    _loadSpecialistTexture(name) {
-      this.SPECIALIST_TEXTURES[name] = Texture.from(new URL(`../assets/specialists/${name}.svg`, import.meta.url).href)
-      //disable mipmap
-      this.SPECIALIST_TEXTURES[name].baseTexture.mipmap = 0
-    }
+  _loadSpecialistTexture(name: string) {
+    this.SPECIALIST_TEXTURES[name] = Texture.from(new URL(`../assets/specialists/${name}.svg`, import.meta.url).href)
+    //disable mipmap
+    this.SPECIALIST_TEXTURES[name].baseTexture.mipmap = 0
+  }
 
-    getSpecialistTexture(specialistKey) {
-      return this.SPECIALIST_TEXTURES[specialistKey]
-    }
+  getSpecialistTexture(specialistKey: string) {
+    return this.SPECIALIST_TEXTURES[specialistKey]
+  }
 
-    _loadPlayerSymbols() {
-      this.PLAYER_SYMBOLS['circle'] = [
-        Texture.from(new URL('../assets/map-objects/256x256_circle.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/256x256_circle_warp_gate.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/256x256_circle_partial.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/256x256_circle_partial_warp_gate.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/128x128_circle_carrier.svg', import.meta.url).href)
-      ]
-      this.PLAYER_SYMBOLS['hexagon'] = [
-        Texture.from(new URL('../assets/map-objects/256x256_hexagon.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/256x256_hexagon_warp_gate.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/256x256_hexagon_partial.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/256x256_hexagon_partial_warp_gate.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/128x128_hexagon_carrier.svg', import.meta.url).href),
-      ]
-      this.PLAYER_SYMBOLS['diamond'] = [
-        Texture.from(new URL('../assets/map-objects/256x256_diamond.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/256x256_diamond_warp_gate.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/256x256_diamond_partial.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/256x256_diamond_partial_warp_gate.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/128x128_diamond_carrier.svg', import.meta.url).href),
-      ]
-      this.PLAYER_SYMBOLS['square'] = [
-        Texture.from(new URL('../assets/map-objects/256x256_square.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/256x256_square_warp_gate.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/256x256_square_partial.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/256x256_square_partial_warp_gate.svg', import.meta.url).href),
-        Texture.from(new URL('../assets/map-objects/128x128_square_carrier.svg', import.meta.url).href)
-      ]
-    }
+  _loadPlayerSymbols() {
+    this.PLAYER_SYMBOLS['circle'] = TEXTURE_URLS.CIRCLES.map(u => Texture.from(u));
+    this.PLAYER_SYMBOLS['hexagon'] = TEXTURE_URLS.HEXAGONS.map(u => Texture.from(u));
+    this.PLAYER_SYMBOLS['diamond'] = TEXTURE_URLS.DIAMONDS.map(u => Texture.from(u));
+    this.PLAYER_SYMBOLS['square'] = TEXTURE_URLS.SQUARES.map(u => Texture.from(u));
+  }
 
-    _loadStarSymbols() {
-      this.STAR_SYMBOLS['scannable'] = Texture.from(new URL('../assets/map-objects/128x128_star_scannable.svg', import.meta.url).href)
-      this.STAR_SYMBOLS['unscannable'] = Texture.from(new URL('../assets/map-objects/128x128_star_unscannable.svg', import.meta.url).href)
-      this.STAR_SYMBOLS['home'] = Texture.from(new URL('../assets/map-objects/128x128_star_home.svg', import.meta.url).href)
-      this.STAR_SYMBOLS['black_hole'] = Texture.from(new URL('../assets/map-objects/128x128_star_black_hole.svg', import.meta.url).href)
-      this.STAR_SYMBOLS['black_hole_binary'] = Texture.from(new URL('../assets/map-objects/128x128_star_black_hole_binary.svg', import.meta.url).href)
-      this.STAR_SYMBOLS['binary_scannable'] = Texture.from(new URL('../assets/map-objects/128x128_star_scannable_binary.svg', import.meta.url).href)
-      this.STAR_SYMBOLS['binary_unscannable'] = Texture.from(new URL('../assets/map-objects/128x128_star_unscannable_binary.svg', import.meta.url).href)
-    }
+  _loadStarSymbols() {
+    this.STAR_SYMBOLS['scannable'] = Texture.from(TEXTURE_URLS.STAR_SCANNABLE)
+    this.STAR_SYMBOLS['unscannable'] = Texture.from(TEXTURE_URLS.STAR_UNSCANNABLE)
+    this.STAR_SYMBOLS['home'] = Texture.from(TEXTURE_URLS.HOME)
+    this.STAR_SYMBOLS['black_hole'] = Texture.from(TEXTURE_URLS.BLACK_HOLE)
+    this.STAR_SYMBOLS['black_hole_binary'] = Texture.from(TEXTURE_URLS.BLACK_HOLE_BINARY)
+    this.STAR_SYMBOLS['binary_scannable'] = Texture.from(TEXTURE_URLS.BINARY_SCANNABLE)
+    this.STAR_SYMBOLS['binary_unscannable'] = Texture.from(TEXTURE_URLS.BINARY_UNSCANNABLE)
+  }
 
-    getRandomStarNebulaTexture(seed) {
-      TextureService.seededRNG.seed(seed+'n')
-      let index = Math.floor(TextureService.seededRNG.random() * this.STAR_NEBULA_TEXTURES.length)
+  getRandomStarNebulaTexture(seed) {
+    TextureService.seededRNG.seed(seed + 'n')
+    let index = Math.floor(TextureService.seededRNG.random() * this.STAR_NEBULA_TEXTURES.length)
 
-      return this.STAR_NEBULA_TEXTURES[index]
-    }
+    return this.STAR_NEBULA_TEXTURES[index]
+  }
 
-    getRandomStarAsteroidFieldTexture(seed) {
-      TextureService.seededRNG.seed(seed+'a')
-      let index = Math.floor(TextureService.seededRNG.random() * this.STAR_ASTEROID_FIELD_TEXTURES.length)
+  getRandomStarAsteroidFieldTexture(seed) {
+    TextureService.seededRNG.seed(seed + 'a')
+    let index = Math.floor(TextureService.seededRNG.random() * this.STAR_ASTEROID_FIELD_TEXTURES.length)
 
-      return this.STAR_ASTEROID_FIELD_TEXTURES[index]
-    }
+    return this.STAR_ASTEROID_FIELD_TEXTURES[index]
+  }
 
-    getRandomWormholeTexture () {
-      // TODO: More textures?
-      return this.STAR_WORMHOLE_TEXTURES[0]
-    }
+  getRandomWormholeTexture() {
+    // TODO: More textures?
+    return this.STAR_WORMHOLE_TEXTURES[0]
+  }
 }
 
 export default new TextureService()
