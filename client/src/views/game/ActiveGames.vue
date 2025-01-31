ebe6423935fa2d36df7c274e9b8927cc6b8b0ae4
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2025-01-31 19:19:26 +0100

    Show open lobbies in active games page

--- client/src/views/game/ActiveGames.vue
+++ client/src/views/game/ActiveGames.vue
@@ -15,6 +15,7 @@
 
     <active-games/>
     <hr/>
+    <open-games />
     <spectating-games/>
     <completed-games/>
 
@@ -27,9 +28,11 @@ import ViewTitle from '../components/ViewTitle.vue'
 import ActiveGamesVue from './components/gameList/ActiveGames.vue'
 import CompletedGamesVue from './components/gameList/CompletedGames.vue'
 import SpectatingGamesVue from './components/gameList/SpectatingGames.vue'
+import OpenGames from "@/views/game/components/gameList/OpenGames.vue";
 
 export default {
   components: {
+    OpenGames,
     'view-container': ViewContainer,
     'view-title': ViewTitle,
     'active-games': ActiveGamesVue,
