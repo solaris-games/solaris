36663e75f75d12a6a20b3ed554e201878fbb4e0a
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2025-05-27 21:56:10 +0200

    Fix waypoint out of range causing error

--- client/src/views/game/components/carrier/CarrierWaypoints.vue
+++ client/src/views/game/components/carrier/CarrierWaypoints.vue
@@ -216,7 +216,7 @@ export default {
       this.recalculateTotalEta()
       this.recalculateLooped()
     },
-    onWaypointOutOfRange ({ waypoint }) {
+    onWaypointOutOfRange () {
       this.$toast.error(`This waypoint is out of hyperspace range.`)
     },
     recalculateTotalEta () {
