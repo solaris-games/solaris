40744fac0b612ab223cd0f0064730a9d28da5237
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2025-06-17 20:38:28 +0200

    Port announcement components to typescript and fix announcement state types

--- client/src/views/Announcements.vue
+++ client/src/views/Announcements.vue
@@ -5,16 +5,16 @@
       :showSocialLinks="true"
     />
 
-    <loading-spinner v-if="!announcementState || !announcements" />
+    <loading-spinner :loading="!announcementState || !announcements" />
 
-    <div v-else>
+    <div v-if="announcements">
       <p>
         <a href="https://discord.com/invite/v7PD33d" target="_blank" title="Discord" style="text-decoration: none;">
           Join the Discord
         </a>to stay up-to-date, discuss the game and chat with the community!
       </p>
 
-      <announcement
+      <announcement-panel
         v-for="announcement in announcements"
         :key="announcement._id"
         :announcement="announcement"
@@ -24,57 +24,50 @@
   </view-container>
 </template>
 
-<script>
-
+<script setup lang="ts">
 import ViewContainer from "./components/ViewContainer.vue";
 import ViewTitle from "./components/ViewTitle.vue";
 import LoadingSpinner from "./components/LoadingSpinner.vue";
-import Announcement from "./components/Announcement.vue";
-import AnnouncementsApiService from "../services/api/announcements";
+import AnnouncementPanel from "./components/Announcement.vue";
+import { ref, onMounted, inject, type Ref } from 'vue';
+import type {Announcement, AnnouncementState} from "@solaris-common";
+import {getAnnouncementState, getCurrentAnnouncements} from "@/services/typedapi/announcement";
+import {formatError, httpInjectionKey, isOk} from "@/services/typedapi";
+import {toastInjectionKey} from "@/util/keys";
 
-export default {
-  name: "Announcements",
-  components: {
-    'view-container': ViewContainer,
-    'view-title': ViewTitle,
-    'loading-spinner': LoadingSpinner,
-    'announcement': Announcement
-  },
-  data() {
-    return {
-      announcements: null,
-      announcementState: null
-    }
-  },
-  async mounted () {
-    const resp1 = await AnnouncementsApiService.getAnnouncementState();
+const httpClient = inject(httpInjectionKey)!;
+const toast = inject(toastInjectionKey)!;
 
-    if (resp1.status === 200) {
-      this.announcementState = resp1.data;
+const announcements: Ref<Announcement<string>[] | null> = ref(null);
+const announcementState: Ref<AnnouncementState<string> | null> = ref(null);
 
-      const resp2 = await AnnouncementsApiService.getCurrentAnnouncements();
-
-      if (resp2.status === 200) {
-        this.announcements = resp2.data;
-
-        await AnnouncementsApiService.markAsRead();
-      } else {
-        console.error(resp2);
-      }
-    } else {
-      console.error(resp1);
-    }
-  },
-  methods: {
-    isUnread (announcement) {
-      if (!announcement || !this.announcementState) {
-        return false;
-      }
-
-      return this.announcementState.unreadAnnouncements.includes(announcement._id);
-    }
+const isUnread = (announcement: Announcement<string>) => {
+  if (!announcementState.value) {
+    return false;
   }
-}
+
+  return announcementState.value.unreadAnnouncements.includes(announcement._id);
+};
+
+onMounted(async () => {
+  const resp1 = await getCurrentAnnouncements(httpClient)();
+
+  if (isOk(resp1)) {
+    announcements.value = resp1.data;
+
+    const resp2 = await getAnnouncementState(httpClient)();
+
+    if (isOk(resp2)) {
+      announcementState.value = resp2.data;
+    } else {
+      console.error(formatError(resp2));
+      toast.error("Failed to load announcement state");
+    }
+  } else {
+    console.error(formatError(resp1));
+    toast.error("Failed to load announcements");
+  }
+});
 </script>
 
 <style scoped>
