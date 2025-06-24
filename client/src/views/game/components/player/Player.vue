b725795bef2602bcbdd4a84c4e6eb607e005c9d1
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2025-06-24 20:30:46 +0200

    Port Player component to typescript

--- client/src/views/game/components/player/Player.vue
+++ client/src/views/game/components/player/Player.vue
@@ -7,7 +7,7 @@
         <i class="fas fa-user-friends ms-1" v-if="user.roles.communityManager" title="This player is an active community manager"></i>
         <i class="fas fa-dice ms-1" v-if="user.roles.gameMaster" title="This player is an active game master"></i>
       </span>
-      <elo-rating v-if="is1v1Game" :user="user" class="me-2"/>
+      <elo-rating v-if="user && is1v1Game" :user="user" class="me-2"/>
       <button @click="onOpenPrevPlayerDetailRequested" class="btn btn-sm btn-outline-info"><i class="fas fa-chevron-left"></i></button>
       <button @click="onOpenNextPlayerDetailRequested" class="btn btn-sm btn-outline-info ms-1"><i class="fas fa-chevron-right"></i></button>
       <button @click="panToPlayer" class="btn btn-sm btn-outline-info ms-1"><i class="fas fa-eye"></i></button>
@@ -23,29 +23,28 @@
 
     <infrastructure v-if="player" :playerId="player._id"/>
 
-    <yourInfrastructure v-if="userPlayer && player != userPlayer"
+    <yourInfrastructure v-if="player && userPlayer && player != userPlayer"
                     :comparePlayerId="player._id"/>
 
     <h4 v-if="player && player.research" class="mt-2">Technology</h4>
 
     <research v-if="player && player.research" :playerId="player._id"/>
 
-    <loading-spinner :loading="player && !player.isOpenSlot && !user"/>
+    <loading-spinner :loading="Boolean(player && !player.isOpenSlot && !user)"/>
 
     <h4 class="mt-2" v-if="canViewAchievements">Achievements</h4>
 
-    <achievements v-if="canViewAchievements"
+    <achievements v-if="canViewAchievements && user"
                     :level="user.achievements.level"
                     :victories="user.achievements.victories"
                     :rank="user.achievements.rank"
                     :renown="user.achievements.renown"/>
 
-    <sendRenown v-if="canSendRenown" :player="player" :userPlayer="userPlayer"
-      @onRenownSent="onRenownSent"/>
+    <sendRenown v-if="userPlayer && player && canSendRenown" :player="player" :userPlayer="userPlayer" />
 
     <h4 class="mt-2" v-if="canAwardBadge">Badges</h4>
 
-    <player-badges v-if="canAwardBadge"
+    <player-badges v-if="player && canAwardBadge"
       :playerId="player._id"
       @onOpenPurchasePlayerBadgeRequested="onOpenPurchasePlayerBadgeRequested"/>
 
@@ -56,8 +55,8 @@
 </div>
 </template>
 
-<script>
-import LoadingSpinnerVue from '../../../components/LoadingSpinner.vue'
+<script setup lang="ts">
+import LoadingSpinner from '../../../components/LoadingSpinner.vue'
 import MenuTitle from '../MenuTitle.vue'
 import Overview from './Overview.vue'
 import Infrastructure from '../shared/Infrastructure.vue'
@@ -66,149 +65,100 @@ import Research from './Research.vue'
 import Achievements from './Achievements.vue'
 import SendRenown from './SendRenown.vue'
 import PlayerBadges from '../badges/PlayerBadges.vue'
-import Reputation from './Reputation.vue'
 import EloRating from './EloRating.vue'
 import PlayerReport from './PlayerReport.vue'
 import gameService from '../../../../services/api/game'
 import GameHelper from '../../../../services/gameHelper'
 import {eventBusInjectionKey} from "@/eventBus";
 import MapCommandEventBusEventNames from "@/eventBusEventNames/mapCommand";
-import { inject } from 'vue';
+import { inject, ref, computed, type Ref, onMounted } from 'vue';
+import type { Player, UserPublic } from '@solaris-common'
+import { useStore, type Store } from 'vuex';
+import type { State } from "@/store";
 
-export default {
-  components: {
-    'loading-spinner': LoadingSpinnerVue,
-    'menu-title': MenuTitle,
-    'overview': Overview,
-    'infrastructure': Infrastructure,
-    'yourInfrastructure': YourInfrastructure,
-    'research': Research,
-    'achievements': Achievements,
-    'sendRenown': SendRenown,
-    'player-badges': PlayerBadges,
-    'reputation': Reputation,
-    'elo-rating': EloRating,
-    'player-report': PlayerReport
-  },
-  props: {
-    playerId: String
-  },
-  setup () {
-    return {
-      eventBus: inject(eventBusInjectionKey)
-    }
-  },
-  data () {
-    return {
-      player: null,
-      user: null,
-      userPlayer: null,
-      playerIndex: 0
-    }
-  },
-  async mounted () {
-    this.player = GameHelper.getPlayerById(this.$store.state.game, this.playerId)
-    this.userPlayer = GameHelper.getUserPlayer(this.$store.state.game)
-    this.playerIndex = this.$store.state.game.galaxy.players.indexOf(this.player)
-    this.leaderboard = GameHelper.getSortedLeaderboardPlayerList(this.$store.state.game)
+const props = defineProps<{
+  playerId: string,
+}>();
+
+const emit = defineEmits<{
+  onCloseRequested: [],
+  onViewCompareIntelRequested: [playerId: string],
+  onViewColourOverrideRequested: [playerId: string],
+  onOpenTradeRequested: [playerId: string],
+  onOpenPurchasePlayerBadgeRequested: [playerId: string],
+  onOpenReportPlayerRequested: [playerId: string],
+  onOpenPlayerDetailRequested: [playerId: string],
+}>();
+
+const eventBus = inject(eventBusInjectionKey)!;
+
+const store: Store<State> = useStore();
+
+const player: Ref<Player<string> | null> = ref(null);
+const user: Ref<UserPublic<string> | null> = ref(null);
+const userPlayer: Ref<Player<string> | null> = ref(null);
+const leaderboard: Ref<Player<string>[] | null> = ref(null);
+const playerIndex = ref(0);
+
+onMounted(async () => {
+  player.value = GameHelper.getPlayerById(store.state.game, props.playerId) || null;
+  userPlayer.value = GameHelper.getUserPlayer(store.state.game) || null;
+  playerIndex.value = store.state.game.galaxy.players.indexOf(player.value);
+  leaderboard.value = GameHelper.getSortedLeaderboardPlayerList(store.state.game)
 
     // If there is a legit user associated with this user then get the
     // user info so we can show more info like achievements.
-    if (this.$store.state.userId && !this.player.isOpenSlot && GameHelper.isNormalAnonymity(this.$store.state.game)) {
+    if (store.state.userId && !player.value!.isOpenSlot && GameHelper.isNormalAnonymity(store.state.game)) {
       try {
-        const response = await gameService.getPlayerUserInfo(this.$store.state.game._id, this.player._id)
+        const response = await gameService.getPlayerUserInfo(store.state.game._id, player.value!._id);
 
-        this.user = response.data
+        user.value = response.data;
       } catch (err) {
         console.error(err)
       }
     }
+});
 
-    if (this.user == null) {
-      this.user = {}
-    }
-  },
-  methods: {
-    onCloseRequested (e) {
-      this.$emit('onCloseRequested', e)
-    },
-    onViewCompareIntelRequested (e) {
-      this.$emit('onViewCompareIntelRequested', e)
-    },
-    onViewColourOverrideRequested (e) {
-      this.$emit('onViewColourOverrideRequested', e);
-    },
-    onOpenTradeRequested (e) {
-      this.$emit('onOpenTradeRequested', this.playerId)
-    },
-    onOpenPurchasePlayerBadgeRequested (e) {
-      this.$emit('onOpenPurchasePlayerBadgeRequested', e)
-    },
-    onOpenReportPlayerRequested (e) {
-      this.$emit('onOpenReportPlayerRequested', e)
-    },
-    panToPlayer (e) {
-      this.eventBus.emit(MapCommandEventBusEventNames.MapCommandPanToPlayer, { player: this.player });
-    },
-    onOpenPrevPlayerDetailRequested (e) {
-      let prevLeaderboardIndex = this.leaderboard.indexOf(this.player) - 1;
+const isGameFinished = computed(() => GameHelper.isGameFinished(store.state.game));
+const isGameStarted = computed(() => GameHelper.isGameStarted(store.state.game));
+const isAnonymousGame = computed(() => store.state.game.settings.general.anonymity === 'extra');
+const is1v1Game = computed(() =>GameHelper.is1v1Game(store.state.game));
+const canViewAchievements = computed(() => player.value && player.value.isRealUser && user.value && user.value.achievements);
+const playerIsUser = computed(() => player.value && userPlayer.value && userPlayer.value._id !== player.value._id);
+const canSendRenown = computed(() => player.value && isGameStarted.value && player.value.isRealUser && userPlayer.value && !playerIsUser.value && (!isAnonymousGame.value || isGameFinished.value));
+const canAwardBadge = computed(() => player.value && player.value.isRealUser && userPlayer.value && !playerIsUser.value);
 
-      if (prevLeaderboardIndex < 0) {
-        prevLeaderboardIndex = this.leaderboard.length - 1;
-      }
+const onCloseRequested = () => emit('onCloseRequested');
+const onViewCompareIntelRequested = (playerId: string) => emit('onViewCompareIntelRequested', playerId);
+const onViewColourOverrideRequested = (playerId: string) => emit('onViewColourOverrideRequested', playerId);
+const onOpenTradeRequested = () => emit('onOpenTradeRequested', props.playerId);
+const onOpenPurchasePlayerBadgeRequested = () => emit('onOpenPurchasePlayerBadgeRequested', props.playerId);
+const onOpenReportPlayerRequested = () => emit('onOpenReportPlayerRequested', props.playerId);
+const panToPlayer = () => eventBus.emit(MapCommandEventBusEventNames.MapCommandPanToPlayer, { player: player.value! });
+const onOpenPlayerDetailRequested = (player: Player<string>) => emit('onOpenPlayerDetailRequested', player._id);
 
-      let prevPlayer = this.leaderboard[prevLeaderboardIndex];
-
-      this.onOpenPlayerDetailRequested(prevPlayer);
-    },
-    onOpenNextPlayerDetailRequested (e) {
-      let nextLeaderboardIndex = this.leaderboard.indexOf(this.player) + 1;
-
-      if (nextLeaderboardIndex > this.leaderboard.length - 1) {
-        nextLeaderboardIndex = 0;
-      }
-
-      let nextPlayer = this.leaderboard[nextLeaderboardIndex];
-
-      this.onOpenPlayerDetailRequested(nextPlayer);
-    },
-    onOpenPlayerDetailRequested (player) {
-      this.$emit('onOpenPlayerDetailRequested', player._id)
-    },
-    onRenownSent (e) {
-      if (this.user.achievements) {
-        this.user.achievements.renown += e
-      }
-    }
-  },
-  computed: {
-    game () {
-      return this.$store.state.game
-    },
-    isGameFinished: function () {
-      return GameHelper.isGameFinished(this.$store.state.game)
-    },
-    isAnonymousGame () {
-      return this.game.settings.general.anonymity === 'extra'
-    },
-    is1v1Game () {
-      return GameHelper.is1v1Game(this.game)
-    },
-    canViewAchievements () {
-      return this.player && this.player.isRealUser && this.user && this.user.achievements
-    },
-    canSendRenown () {
-      if (this.isAnonymousGame) {
-        return this.player && this.game.state.startDate && this.player.isRealUser && this.userPlayer && this.player != this.userPlayer && this.isGameFinished
-      } else {
-        return this.player && this.game.state.startDate && this.player.isRealUser && this.userPlayer && this.player != this.userPlayer
-      }
-    },
-    canAwardBadge () {
-      return this.player && this.player.isRealUser && this.userPlayer && this.player != this.userPlayer
-    }
+const onOpenPrevPlayerDetailRequested = () => {
+  let prevLeaderboardIndex = leaderboard.value!.indexOf(player.value!) - 1;
+  if (prevLeaderboardIndex < 0) {
+    prevLeaderboardIndex = leaderboard.value!.length - 1;
   }
-}
+
+  const prevPlayer = leaderboard.value![prevLeaderboardIndex];
+
+  onOpenPlayerDetailRequested(prevPlayer);
+};
+
+const onOpenNextPlayerDetailRequested = () => {
+  let nextLeaderboardIndex = leaderboard.value!.indexOf(player.value!) + 1;
+
+  if (nextLeaderboardIndex > leaderboard.value!.length - 1) {
+    nextLeaderboardIndex = 0;
+  }
+
+  const nextPlayer = leaderboard.value![nextLeaderboardIndex];
+
+  onOpenPlayerDetailRequested(nextPlayer);
+};
 </script>
 
 <style scoped>
