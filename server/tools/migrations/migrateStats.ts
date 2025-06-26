3f8bb8d41293a5b40ddcf70e7212e214a4f4cd73
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2025-06-26 19:31:07 +0200

    Implement stats migration

--- server/tools/migrations/migrateStats.ts
+++ server/tools/migrations/migrateStats.ts
@@ -1,5 +1,8 @@
 import {JobParameters} from "../tool";
-import {AwardedBadge} from "@solaris-common";
+import {AwardedBadge, StatsSlice, UserAchievements} from "@solaris-common";
+import {User} from "../../services/types/User";
+import {DBObjectId} from "../../services/types/DBObjectId";
+import {Statistics} from "solaris-common/dist/api/types/common/stats";
 
 type OldUserAchievements<ID> = {
     victories: number;
@@ -67,6 +70,59 @@ type OldUserAchievements<ID> = {
     },
 }
 
+const moveStats = (user: User) => {
+    // @ts-ignore
+    const oldAchievements = user.achievements as OldUserAchievements<DBObjectId>;
+
+    const stats: Statistics = {
+        trade: oldAchievements.trade,
+        research: oldAchievements.research,
+        combat: oldAchievements.combat,
+        infrastructure: oldAchievements.infrastructure,
+    };
+
+    const newAchievements: UserAchievements<DBObjectId> = {
+        stats,
+        victories: oldAchievements.victories,
+        victories1v1: oldAchievements.victories1v1,
+        level: oldAchievements.level,
+        rank: oldAchievements.rank,
+        eloRating: oldAchievements.eloRating,
+        renown: oldAchievements.renown,
+        joined: oldAchievements.joined,
+        completed: oldAchievements.completed,
+        quit: oldAchievements.quit,
+        defeated: oldAchievements.defeated,
+        defeated1v1: oldAchievements.defeated1v1,
+        afk: oldAchievements.afk,
+        badges: oldAchievements.badges,
+    };
+
+    return {
+        updateOne: {
+            filter: {
+                _id: user._id,
+            },
+            update: {
+                $set: {
+                    'achievements': newAchievements,
+                }
+            }
+        }
+    };
+};
+
+const createStatsSlice = (user: User): StatsSlice<DBObjectId> => {
+    const stats: Statistics = user.achievements.stats;
+
+    return {
+        stats,
+        userId: user._id,
+        processed: false,
+        gameId: undefined,
+    };
+};
+
 export const migrateStats = async (ctx: JobParameters) => {
     const statsSliceRepo = ctx.container.statisticsService.statsSliceRepository;
 
@@ -85,10 +141,30 @@ export const migrateStats = async (ctx: JobParameters) => {
             'achievements': 1,
         }, { _id: 1 }, pageSize, page * pageSize);
 
-        
+        const writes = users.map(moveStats);
+
+        await userRepository.bulkWrite(writes);
 
         log.info(`Page ${page}/${totalPages}`);
 
         page++;
     } while (page <= totalPages)
+
+    log.info("Finished migrating stats in user data");
+
+    do {
+        const users = await userRepository.find({}, {
+            'achievements': 1,
+        }, { _id: 1 }, pageSize, page * pageSize);
+
+        const statsWrites = users.map(createStatsSlice);
+
+        await statsSliceRepo.bulkWrite(statsWrites);
+
+        log.info(`Page ${page}/${totalPages}`);
+
+        page++;
+    } while (page <= totalPages)
+
+    log.info("Finished migrating stats slices");
 };
