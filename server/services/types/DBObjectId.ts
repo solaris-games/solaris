0f77478c060d3be49c1a84a84d21a74b9f9f57ff
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2025-10-06 16:32:35 +0200

    Remove explicit method declarations

--- server/services/types/DBObjectId.ts
+++ server/services/types/DBObjectId.ts
@@ -1,10 +1,7 @@
-import mongoose, { ObjectId } from "mongoose";
+import mongoose from "mongoose";
 
 export interface DBObjectId extends mongoose.Types.ObjectId {
-    // equals(id: DBObjectId): boolean; -- Note: We never use this as we cannot ensure that anything that comes through the API layer via params are mongo object IDs unless we explicitly cast them.
-    getTimestamp(): Date;
-    toString(): string;
-};
+}
 
 export const objectId = (): DBObjectId => new mongoose.Types.ObjectId() as any;
 
