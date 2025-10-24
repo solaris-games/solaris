808b599a7c874b080c9051fe6a7764180091d439
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2025-10-24 16:39:10 +0200

    Fix middleware for old services

--- client/src/services/api/base.js
+++ client/src/services/api/base.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 import router from '../../router';
+import {buildIdempotencyKey, isUnsafeMethod} from "@/util/http";
 
 class BaseApiService {
   constructor () {
     this.BASE_URL = import.meta.env.VUE_APP_API_HOST + '/api/'
 
     axios.interceptors.request.use(config => {
-      config.headers['Idempotency-Key'] = this.buildIdempotencyKey();
+      config.headers['Idempotency-Key'] = buildIdempotencyKey();
 
       return config;
     },
     error => {
       return Promise.reject(error);
-    }, { synchronous: true, runWhen: this.isUnsafeMethod });
+    }, { synchronous: true, runWhen: isUnsafeMethod });
 
     axios.interceptors.response.use(
       response => {
--- client/src/util/http.ts
+++ client/src/util/http.ts
@@ -18,13 +18,13 @@ export const convertDates = (obj: unknown) => {
   return obj;
 };
 
-const isUnsafeMethod = (config) => {
+export const isUnsafeMethod = (config) => {
   // Rather than just going for idempotent methods, ie the safe ones and put and delete,
   // We are electing here to NOT trust that put or delete currently behave in an idempotent way on the server...
   return !['get', 'head', 'options', 'trace'].includes(config.method);
 };
 
-const buildIdempotencyKey = () => {
+export const buildIdempotencyKey = () => {
   return `${generateV7Uuid()}`;
 };
 
