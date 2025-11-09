62e43a35371f15f9464f4a3fe96e19782715ad8a
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2025-11-09 18:39:53 +0100

    Add migration for wormholes for old game histories

--- server/tools/migrate.ts
+++ server/tools/migrate.ts
@@ -3,12 +3,14 @@ import {migrateBadges} from "./migrations/migrateBadges";
 import {migrateCombatResolution} from "./migrations/migrateCombatResolution";
 import {migrateStats} from "./migrations/migrateStats";
 import { migrateInitialGameState } from "./migrations/migrateInitialGameState";
+import {migrateWormholesHistory} from "./migrations/migrateWormholesHistory";
 
 const MIGRATIONS = {
     "2025-02-05-badges": migrateBadges,
     "2025-03-29-combat-resolution": migrateCombatResolution,
     "2025-06-26-stats": migrateStats,
     "2025-10-19-initial-game-state": migrateInitialGameState,
+    "2025-11-08-wormholes-history": migrateWormholesHistory,
 };
 
 const job = makeJob('Migration', async (ctx) => {
