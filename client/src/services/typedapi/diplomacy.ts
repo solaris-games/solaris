0192884a7b6d40483a9bf29cb1e86ce4e3b7bb11
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2025-12-09 21:10:49 +0100

    Fix missing credentials for diplomacy API

--- client/src/services/typedapi/diplomacy.ts
+++ client/src/services/typedapi/diplomacy.ts
@@ -5,21 +5,21 @@ import {doGet, doPut, type ResponseResult} from "@/services/typedapi/index";
 const routes = createDiplomacyRoutes<string>();
 
 export const listDiplomacy = (axios: Axios) => async (gameId: string): Promise<ResponseResult<DiplomaticStatus<string>[]>> => {
-  return doGet(axios)(routes.listDiplomacy, { gameId }, {});
+  return doGet(axios)(routes.listDiplomacy, { gameId }, {}, { withCredentials: true });
 }
 
 export const detailDiplomacy = (axios: Axios) => async (gameId: string, playerId: string): Promise<ResponseResult<DiplomaticStatus<string>>> => {
-  return doGet(axios)(routes.detailDiplomacy, { gameId, toPlayer: playerId }, {});
+  return doGet(axios)(routes.detailDiplomacy, { gameId, toPlayer: playerId }, {}, { withCredentials: true });
 }
 
 export const ally = (axios: Axios) => async (gameId: string, playerId: string): Promise<ResponseResult<DiplomaticStatus<string>>> => {
-  return doPut(axios)(routes.ally, { gameId, playerId }, {}, {});
+  return doPut(axios)(routes.ally, { gameId, playerId }, {}, {}, { withCredentials: true });
 }
 
 export const enemy = (axios: Axios) => async (gameId: string, playerId: string): Promise<ResponseResult<DiplomaticStatus<string>>> => {
-  return doPut(axios)(routes.enemy, { gameId, playerId }, {}, {});
+  return doPut(axios)(routes.enemy, { gameId, playerId }, {}, {}, { withCredentials: true });
 }
 
 export const neutral = (axios: Axios) => async (gameId: string, playerId: string): Promise<ResponseResult<DiplomaticStatus<string>>> => {
-  return doPut(axios)(routes.neutral, { gameId, playerId }, {}, {});
+  return doPut(axios)(routes.neutral, { gameId, playerId }, {}, {}, { withCredentials: true });
 }
