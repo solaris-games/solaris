7da2d3f7bd8f5e1dce60f52ef7210390d9148418
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2025-12-20 14:48:39 +0100

    Port client side usage of research API

---
+++ client/src/services/typedapi/resesarch.ts
@@ -0,0 +1,18 @@
+import {
+  createResearchRoutes,
+  type ResearchType,
+  type ResearchTypeNotRandom,
+  type ResearchUpdateResponse
+} from "@solaris-common";
+import {doPut, type ResponseResult} from "@/services/typedapi/index";
+import type { Axios } from "axios";
+
+const routes = createResearchRoutes<string>();
+
+export const updateResesarchNow = (axios: Axios) => async (gameId: string, research: ResearchTypeNotRandom): Promise<ResponseResult<ResearchUpdateResponse>> => {
+  return doPut(axios)(routes.updateNow, { gameId }, {}, { preference: research }, { withCredentials: true });
+};
+
+export const updateResesarchNext = (axios: Axios) => async (gameId: string, research: ResearchType): Promise<ResponseResult<ResearchUpdateResponse>> => {
+  return doPut(axios)(routes.updateNext, { gameId }, {}, { preference: research }, { withCredentials: true });
+};
