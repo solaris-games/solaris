ee6b031877ea984774be565cc9857fae6ef20697
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2026-03-01 02:17:36 +0100

    Fix scuttling display on frontend

--- client/src/views/game/components/carrier/CarrierDetail.vue
+++ client/src/views/game/components/carrier/CarrierDetail.vue
@@ -23,7 +23,7 @@
     </div>
 
     <div class="row bg-danger" v-if="carrier.isScuttled">
-      <div class="col text-center pt-2">
+      <div class="col text-center p-2">
         <p class="mb-0">This carrier and all its ships will be scuttled.</p>
       </div>
     </div>
@@ -443,9 +443,7 @@ const confirmScuttleCarrier = async () => {
   if (isOk(response)) {
     toast.default(`${carrier.value.name} has been scuttled. All ships will be destroyed.`);
 
-    store.commit('gameCarrierScuttled', {
-      carrierId: carrier.value._id
-    });
+    carrier.value.isScuttled = true;
 
     AudioService.leave();
 
