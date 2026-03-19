a05de6664a9c514d7aba18116eb6500ed349b234
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2026-03-19 16:53:12 +0100

    Port more components

--- client/src/views/game/components/player/StatisticRow.vue
+++ client/src/views/game/components/player/StatisticRow.vue
@@ -1,113 +1,64 @@
 <template>
     <tr>
       <td>{{header}}</td>
-      <td class="text-end" :title="isPlayerStatUncertain ? scanningRangeTooltip : null">{{formatValue(player, playerStat)}}{{ isPlayerStatUncertain ? '?' : ''}}</td>
-      <td class="text-end" v-if="userIsInGame() && !isUserPlayer()"
+      <td class="text-end" :title="isPlayerStatUncertain ? scanningRangeTooltip : undefined">{{formatValue(player, playerStat)}}{{ isPlayerStatUncertain ? '?' : ''}}</td>
+      <td class="text-end" v-if="userIsInGame && !isUserPlayer && userPlayerStat && userPlayer"
         :class="{'text-danger': playerStat > userPlayerStat,
                   'text-success': playerStat < userPlayerStat}">{{formatValue(userPlayer, userPlayerStat)}}</td>
     </tr>
 </template>
 
-<script>
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useStore } from 'vuex';
 import GameHelper from '../../../../services/gameHelper'
+import type {Player} from "@/types/game";
 
-export default {
-  props: {
-    playerId: {
-      type: String,
-      required: true
-    },
-    header: {
-      type: String,
-      required: true
-    },
-    scanningRangeTooltip: {
-      type: String,
-      required: true
-    },
-    isPlayerStatAlwaysUncertain: {
-      type: Boolean,
-      required: false
-    },
-    playerStat: {
-      type: Number,
-      required: true
-    },
-    userPlayerStat: {
-      type: Number,
-      required: false
-    },
-    formatFunction: {
-      type: Function
-    }
-  },
-  methods: {
-    isUserPlayer () {
-      return this.userPlayer && this.userPlayer._id === this.player._id
-    },
-    userIsInGame () {
-      return this.userPlayer != null
-    },
-    formatValue(player, value) {
-      if (this.formatFunction != null) {
-        return this.formatFunction(player, value);
-      }
+const props = defineProps<{
+  playerId: string,
+  header: string,
+  scanningRangeTooltip: string,
+  isPlayerStatAlwaysUncertain?: boolean,
+  playerStat: number,
+  userPlayerStat?: number,
+  formatFunction?: (p: Player, n: number) => string,
+}>();
 
-      return value;
-    }
-  },
-  computed: {
-    player () {
-        return GameHelper.getPlayerById(this.$store.state.game, this.playerId)
-    },
-    userPlayer () {
-        return GameHelper.getUserPlayer(this.$store.state.game)
-    },
-    isSpecialistsEnabled () {
-      return GameHelper.isSpecialistsEnabled(this.$store.state.game)
-    },
-    isDarkModeExtra () {
-      return GameHelper.isDarkModeExtra(this.$store.state.game)
-    },
-    isConquestHomeStars () {
-      return GameHelper.isConquestHomeStars(this.$store.state.game)
-    },
-    playerIncome () {
-      return GameHelper.calculateIncome(this.$store.state.game, this.player)
-    },
-    userPlayerIncome () {
-      return GameHelper.calculateIncome(this.$store.state.game, this.userPlayer)
-    },
-    playerTickIncome() {
-      return GameHelper.calculateTickIncome(this.$store.state.game, this.player)
-    },
-    userPlayerTickIncome() {
-      return GameHelper.calculateTickIncome(this.$store.state.game, this.userPlayer)
-    },
-    isDarkModeExtra() {
-      return GameHelper.isDarkModeExtra(this.$store.state.game);
-    },
-    hasPerspective() {
-      if (this.userPlayer) {
-        return false
-      }
-
-      return this.player.hasPerspective || false;
-    },
-    isPlayerStatUncertain() {
-      // The stat is uncertain if all of the following are true:
-      // * the game is unfinished
-      // * the user does not have perspective of the other player (ie they're not spectating them)
-      //     - (hasPerspective will be false if the user is a player in the game even if the other player has added them as a spectator.)
-      // * the stat is marked to always be uncertain, or we're in extra dark
-      // * the user is not the player.
-      return !GameHelper.isGameFinished(this.$store.state.game)
-        && !this.hasPerspective
-        && (this.isPlayerStatAlwaysUncertain || this.isDarkModeExtra)
-        && !this.isUserPlayer();
-    }
+const store = useStore();
+const game = computed(() => store.state.game);
+const player = computed(() => GameHelper.getPlayerById(game.value, props.playerId)!);
+const userPlayer = computed(() => GameHelper.getUserPlayer(game.value));
+const userIsInGame = computed(() => Boolean(userPlayer.value));
+const isUserPlayer = computed(() => userPlayer.value && props.playerId === userPlayer.value._id);
+const isDarkModeExtra = computed(() => GameHelper.isDarkModeExtra(game.value));
+const hasPerspective = computed(() => {
+  if (userIsInGame.value) {
+    return false;
   }
+
+  return player.value.hasPerspective || false;
+})
+
+const formatValue = (player: Player, value: number) => {
+  if (props.formatFunction) {
+    return props.formatFunction(player, value);
+  }
+
+  return value;
 }
+
+const isPlayerStatUncertain = computed(() => {
+  // The stat is uncertain if all of the following are true:
+  // * the game is unfinished
+  // * the user does not have perspective of the other player (ie they're not spectating them)
+  //     - (hasPerspective will be false if the user is a player in the game even if the other player has added them as a spectator.)
+  // * the stat is marked to always be uncertain, or we're in extra dark
+  // * the user is not the player.
+  return !GameHelper.isGameFinished(game.value)
+    && !hasPerspective.value
+    && (props.isPlayerStatAlwaysUncertain || isDarkModeExtra.value)
+    && !isUserPlayer.value;
+});
 </script>
 
 <style scoped>
