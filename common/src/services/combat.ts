b813dbf3a73355bb12097a1e9470385cd26d9580
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2026-04-08 12:12:33 +0200

    Prepare combat grouping

--- common/src/services/combat.ts
+++ common/src/services/combat.ts
@@ -2,6 +2,7 @@ import type {Id} from "../types/id";
 import type {CombatGroupService} from "./combatGroup";
 import type {Star} from "../types/common/star";
 import type {Carrier} from "../types/common/carrier";
+import {Game} from "../types/common/game";
 
 export class CombatService<ID extends Id> {
     combatGroupService: CombatGroupService<ID>;
@@ -10,11 +11,19 @@ export class CombatService<ID extends Id> {
         this.combatGroupService = combatGroupService;
     }
 
-    performStar(star: Star<ID>, carriers: Carrier<ID>[]) {
+    performStar(game: Game<ID>, star: Star<ID>, carriers: Carrier<ID>[]) {
+        const playerIds = new Set<ID>([star.ownedByPlayerId!]);
+
+        carriers.forEach((c) => playerIds.add(c.ownedByPlayerId!));
+
+        const players = Array.from(playerIds, (p) => game.galaxy.players.find(pl => pl._id === p)!);
+
+        const combatGroups = this.combatGroupService.computeCombatGroups(game, players);
+
 
     }
 
-    performCarrier(carriers: Carrier<ID>[]) {
+    performCarrier(game: Game<ID>, carriers: Carrier<ID>[]) {
 
     }
 }
