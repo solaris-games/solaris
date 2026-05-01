f791605ff2c5a07d8c68a23174ed2af2425268d6
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2026-05-01 23:10:07 +0200

    WIP advanced combat calculator

--- client/src/views/game/components/combatcalculator/CalculatorCombatGroup.vue
+++ client/src/views/game/components/combatcalculator/CalculatorCombatGroup.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="group-box" :class="hasError ? 'group-box-error' : 'group-box-ok'">
     <div>
+
+    </div>
+    <div class="group-box-group">
+      <button class="btn btn-success" :disabled="otherGroupHasStar" @click="addStar">
+        Add Star
+      </button>
+      <button class="btn btn-success" @click="addCarrier">
+        Add Carrier
+      </button>
       <button class="btn btn-danger" @click="remove">
         Delete
       </button>
@@ -14,20 +23,45 @@
 import { computed } from 'vue';
 import type {CombatBaseCarrier, CombatBasePlayer, CombatBaseStar, CombatGroup} from "@solaris/common";
 
+type CG = CombatGroup<string, CombatBasePlayer<string>, CombatBaseStar<string>, CombatBaseCarrier<string>>;
+
 const props = defineProps<{
   validationErrors: string[],
+  groups: CG[];
 }>();
 
 const emit = defineEmits<{
-  onGroupRemove: [group: CombatGroup<string, CombatBasePlayer<string>, CombatBaseStar<string>, CombatBaseCarrier<string>>],
+  onGroupRemove: [group: CG],
 }>();
 
-const model = defineModel<CombatGroup<string, CombatBasePlayer<string>, CombatBaseStar<string>, CombatBaseCarrier<string>>>();
+const model = defineModel<CG>({ required: true });
 
 const hasError = computed(() => props.validationErrors.length > 0);
 
 const remove = () => emit('onGroupRemove', model.value!);
 
+const otherGroupHasStar = computed(() => Boolean(props.groups.find((g) => g !== model.value && g.star)));
+
+const addStar = () => {
+  model.value!.star = {
+    _id: "star",
+    ships: 0,
+    specialistId: null,
+    ownedByPlayerId: null,
+  };
+};
+
+const addCarrier = () => {
+  const idx = model.value!.carriers.length;
+
+  model.value!.carriers.push({
+    _id: `carrier${idx}`,
+    ships: 0,
+    specialistId: null,
+    specialistTargetedPlayers: [],
+    ownedByPlayerId: null,
+  });
+};
 </script>
 <style scoped>
 .group-box {
@@ -43,5 +77,10 @@ const remove = () => emit('onGroupRemove', model.value!);
 
 .group-box-ok {
   border-color: blue;
+}
+
+.group-box-group {
+  display: flex;
+  flex-direction: row;
 }
 </style>
---
+++ client/src/views/game/components/combatcalculator/CalculatorCombatGroupStar.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="cg-group-star" v-if="model">
+  <p>Hello there! I am a star!</p>
+  <button class="btn btn-sm btn-danger" @click="remove">
+    X
+  </button>
+</div>
+</template>
+<script setup lang="ts">
+import type {CombatBaseStar} from "@solaris/common";
+
+const emit = defineEmits<{
+  onRemove: [],
+}>();
+
+const model = defineModel<CombatBaseStar<string>>({ required: true });
+
+const remove = () => emit('onRemove');
+</script>
+<style scoped>
+.cg-group-star {
+  display: flex;
+}
+</style>
