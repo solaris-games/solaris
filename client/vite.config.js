c7d52cd56b395ea0c47feeab2381c860138cc0ef
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2026-05-15 17:07:33 +0200

    Lift to node 24

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { fileURLToPath } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@solaris/common": path.resolve(__dirname, "../common/src")
+      "@": fileURLToPath(new URL('./src', import.meta.url)),
+      "@solaris/common": fileURLToPath(new URL('../common/src', import.meta.url))
     },
   },
   build: {
