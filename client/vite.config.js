93e442927d4bd9b0ab03982e9f2bda51b1658256
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2026-05-23 18:18:55 +0200

    Add bundle visualizer

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'url'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    process.env.ANALYZE && visualizer({
+      open: true,
+      filename: 'stats.html',
+      gzipSize: true,
+      brotliSize: true,
+      template: 'treemap',
+    }),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": fileURLToPath(new URL('./src', import.meta.url)),
