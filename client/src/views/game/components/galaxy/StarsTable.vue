00e6061f1c8717941525741046e0954759c7d439
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2026-05-29 22:20:19 +0200

    Fix

--- client/src/views/game/components/galaxy/StarsTable.vue
+++ client/src/views/game/components/galaxy/StarsTable.vue
@@ -86,6 +86,7 @@ const sortInfo = useLocalStorage(SORT_INFO_KEY, defaultSortInfo);
 
 const showAll = ref(false);
 const searchFilter = ref('');
+const isGameFinished = computed(() => GameHelper.isGameFinished(game.value));
 const allowUpgrades = ref(store.settings!.interface.galaxyScreenUpgrades === 'enabled' && !isGameFinished.value);
 
 const userPlayer = computed(() => GameHelper.getUserPlayer(game.value));
@@ -94,7 +95,6 @@ const tableData = computed(() => game.value.galaxy.stars);
 const isEconomyEnabled = computed(() => game.value.settings.player.developmentCost.economy !== 'none');
 const isIndustryEnabled = computed(() => game.value.settings.player.developmentCost.industry !== 'none');
 const isScienceEnabled = computed(() => game.value.settings.player.developmentCost.science !== 'none');
-const isGameFinished = computed(() => GameHelper.isGameFinished(game.value));
 
 const filter = (s: Star) => s.name.toLowerCase().includes(searchFilter.value.toLowerCase());
 
