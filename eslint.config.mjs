d6be8a50f37fe4394074f9c600353e0f44620481
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2026-06-29 11:50:24 +0200

    Install eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import configPrettier from "eslint-config-prettier/flat";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  tseslint.configs.recommended,
+  configPrettier,
+  pluginVue.configs["flat/essential"],
+  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+]);
